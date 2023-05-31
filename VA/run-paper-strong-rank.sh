73366deefcce4a8a22555390bb07630437198041
Daniel Friesel <daniel.friesel@uos.de>
2023-05-31 16:10:23 +0200

    VA: cleanup

--- VA/run-paper-strong-rank.sh
+++ VA/run-paper-strong-rank.sh
@@ -8,16 +8,19 @@ set -e
 # -e: number of timed iterations
 # -i; ignored, always uses 262144 elements
 
+(
+
 echo "prim-benchmarks VA strong-rank (dfatool edition)"
 echo "Started at $(date)"
 echo "Revision $(git describe --always)"
 
-# 256 and 512 are not part of upstream config spac
-for nr_dpus in 1 4 16 64 256 512; do
+# 256 and 512 are not part of upstream config space
+for nr_dpus in 512 256 1 4 16 64; do
 	for nr_tasklets in 1 2 4 8 16; do
 		echo
 		if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10; then
 			timeout --foreground -k 1m 30m bin/host_code -w 0 -e 100 -i 2621440 -x 1 || true
 		fi
 	done
-done | tee log-paper-strong-rank.txt
+done
+) | tee log-paper-strong-rank.txt
