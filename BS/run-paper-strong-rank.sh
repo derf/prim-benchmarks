442eff4980b44e35fe939d7dcf2c31f830bbcb62
Daniel Friesel <daniel.friesel@uos.de>
2023-05-31 09:07:37 +0200

    BS: add 256 and 512 DPUs to benchmarks

--- BS/run-paper-strong-rank.sh
+++ BS/run-paper-strong-rank.sh
@@ -12,7 +12,8 @@ echo "prim-benchmarks BS strong-rank (dfatool edition)"
 echo "Started at $(date)"
 echo "Revision $(git describe --always)"
 
-for nr_dpus in 1 4 16 64; do
+# 256 and 512 are not part of upstream
+for nr_dpus in 256 512 1 4 16 64; do
 	for nr_tasklets in 1 2 4 8 16; do
 		echo
 		if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10; then
