ea5352505750377a1de36c1fa12e012dc0d3af4c
Daniel Friesel <daniel.friesel@uos.de>
2023-05-31 12:39:22 +0200

    VA: run.sh: up to 88 threads; strong scaling

--- VA/baselines/cpu/run.sh
+++ VA/baselines/cpu/run.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+HOST="$(hostname)"
+
+echo $HOST
+
+(
+
 echo "prim-benchmarks VA CPU (dfatool edition)"
 echo "Started at $(date)"
 echo "Revision $(git describe --always)"
@@ -10,13 +16,14 @@ echo "Revision $(git describe --always)"
 # default threads: 4
 # default type: int32_t
 
-for nr_threads in 1 2 4 6 8 12 16 20 24 32; do
+for nr_threads in 88 64 44 1 2 4 6 8 12 16 20 24 32; do
 	for i in 16777216 8388608 4194304 33554432 67108864; do
 		for dt in int8_t int16_t int32_t int64_t float double; do
 			if make -B TYPE=${dt}; then
 				# -w 1 to make sure that target array (C) is allocated
-				timeout -k 1m 30m ./va -i ${i} -w 1 -e 100 -t ${nr_threads} || true
+				timeout -k 1m 30m ./va -i ${i} -w 1 -e 100 -t ${nr_threads} -x 1 || true
 			fi
 		done
 	done
 done
+) | tee "${HOST}-explore.txt"
