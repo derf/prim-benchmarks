f438d54780af4ae90638f5d57acdd5e98d6596e9
Daniel Friesel <daniel.friesel@uos.de>
2023-06-02 15:24:54 +0200

    VA cpu: limit to DPU type

--- VA/run-paper-strong-rank.sh
+++ VA/run-paper-strong-rank.sh
@@ -18,7 +18,7 @@ echo "Revision $(git describe --always)"
 for nr_dpus in 512 256 1 4 16 64; do
 	for nr_tasklets in 1 2 4 8 16; do
 		echo
-		if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10; then
+		if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10 verbose=1; then
 			timeout --foreground -k 1m 30m bin/host_code -w 0 -e 100 -i 2621440 -x 1 || true
 		fi
 	done
--- VA/run-paper-weak.sh
+++ VA/run-paper-weak.sh
@@ -8,16 +8,19 @@ set -e
 # -e: number of timed iterations
 # -i; ignored, always uses 262144 elements
 
+(
+
 echo "prim-benchmarks VA weak (dfatool edition)"
 echo "Started at $(date)"
 echo "Revision $(git describe --always)"
 
 # upstream does not include 256 and 512 in config space
-for nr_dpus in 1 4 16 64 256 512; do
+for nr_dpus in 512 256 1 4 16 64; do
 	for nr_tasklets in 1 2 4 8 16; do
 		echo
-		if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10; then
+		if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10 verbose=1; then
 			timeout --foreground -k 1m 30m bin/host_code -w 0 -e 100 -i 2621440 -x 0 || true
 		fi
 	done
-done | tee log-paper-weak.txt
+done
+) | tee log-paper-weak.txt
