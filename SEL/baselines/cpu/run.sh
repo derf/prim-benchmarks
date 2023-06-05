9f4d911c51ef0f8ea59f67b9d34dd49a5abed4b8
Daniel Friesel <daniel.friesel@uos.de>
2023-06-05 11:58:18 +0200

    SEL: add CPU exploration script

--- SEL/baselines/cpu/run.sh
+++ SEL/baselines/cpu/run.sh
@@ -2,16 +2,26 @@
 
 set -e
 
+HOST="$(hostname)"
+
+echo $HOST
+
+(
+
 echo "prim-benchmarks SEL CPU (dfatool edition)"
 echo "Started at $(date)"
 echo "Revision $(git describe --always)"
 
-for nr_threads in 1 2 4 6 8 12 16 20 24 32; do
-	for i in 1258291200 629145600 314572800 157286400 78643200 39321600 19660800; do
-		for dt in uint8_t uint16_t uint32_t uint64_t float double; do
+# default: uint64_t; -t 4 -i 1258291200
+
+for nr_threads in 88 64 44 1 2 4 6 8 12 16 20 24 32; do
+	for i in 19660800 1258291200 629145600 314572800 157286400 78643200 39321600; do
+		#for dt in uint8_t uint16_t uint32_t uint64_t float double; do
+		for dt in uint64_t; do
 			if make -B TYPE=${dt}; then
-				timeout -k 1m 30m ./sel -i ${i} -w 0 -e 100 -t ${nr_threads} || true
+				timeout --foreground -k 1m 60m ./sel -i ${i} -w 0 -e 100 -t ${nr_threads} || true
 			fi
 		done
 	done
 done
+) | tee "${HOST}-explore.txt"
