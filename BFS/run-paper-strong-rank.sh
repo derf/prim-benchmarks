a62d6b0006064f8cd2ec95fc7e39f45416e313a8
Birte Friesel <dfriesel@uos.de>
2023-07-24 11:38:38 +0200

    BFS, BS: remove duplicate configurations

--- BFS/run-paper-strong-rank.sh
+++ BFS/run-paper-strong-rank.sh
@@ -9,7 +9,7 @@ echo "Started at $(date)"
 echo "Revision $(git describe --always)"
 
 # >64 are not part of upstream
-for nr_dpus in 512 256 128 1 4 16 64; do
+for nr_dpus in 128 1 4 16 64; do
 	for nr_tasklets in 1 2 4 8 16; do
 		echo
 		if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} verbose=1; then
