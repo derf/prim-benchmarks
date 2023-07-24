e39651ab9ffe1420dd0bac7d5777f9480b5aa027
Birte Friesel <dfriesel@uos.de>
2023-07-24 11:38:59 +0200

    BS: adjust #DPUs for tinos

--- BS/baselines/cpu/run.sh
+++ BS/baselines/cpu/run.sh
@@ -19,6 +19,7 @@ for nr_threads in 88 64 44 32 24 20 1 2 4 6 8 12 16; do
 	for vs in 262144; do
 		for i in `seq 1 20`; do
 			OMP_NUM_THREADS=${nr_threads} timeout --foreground -k 1m 30m ./bs_omp ${vs} 16777216 || true
+			sleep 10
 		done
 	done
 done
