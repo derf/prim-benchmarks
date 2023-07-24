af3869489b84c2264c29211b29d8baced926ae0d
Birte Friesel <dfriesel@uos.de>
2023-07-24 11:42:22 +0200

    GEMV: update #DPUS

--- GEMV/baselines/cpu/run.sh
+++ GEMV/baselines/cpu/run.sh
@@ -15,5 +15,6 @@ make -B verbose=1
 
 for nr_threads in 88 64 44 1 2 4 6 8 12 16 20 24 32; do
 	OMP_NUM_THREADS=${nr_threads} timeout --foreground -k 1m 30m ./gemv || true
+	sleep 10
 done
 ) | tee "${HOST}-explore.txt"
--- GEMV/run-paper-strong-full.sh
+++ GEMV/run-paper-strong-full.sh
@@ -8,8 +8,8 @@ echo "prim-benchmarks GEMV strong-full (dfatool edition)"
 echo "Started at $(date)"
 echo "Revision $(git describe --always)"
 
-# 2544 is not part of upstream
-for nr_dpus in 2544 256 512 1024 2048; do
+# >2048 are not part of upstream
+for nr_dpus in 2543 2304 256 512 1024 2048; do
 	for nr_tasklets in 1 2 4 8 16; do
 		echo
 		if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10 verbose=1; then
