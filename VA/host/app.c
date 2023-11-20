01c6bf683d000d3cbefcbb2499ac50fbc731db22
Birte Kristina Friesel <birte.friesel@uos.de>
2023-11-20 11:32:41 +0100

    VA: optionally include dpu_{alloc,load,free} overhead in benchmark output

--- VA/host/app.c
+++ VA/host/app.c
@@ -64,25 +64,30 @@ int main(int argc, char **argv) {
     DPU_ASSERT(dpu_probe_init("energy_probe", &probe));
 #endif
 
+#if !WITH_ALLOC_OVERHEAD
     // Allocate DPUs and load binary
     DPU_ASSERT(dpu_alloc(NR_DPUS, NULL, &dpu_set));
     DPU_ASSERT(dpu_load(dpu_set, DPU_BINARY, NULL));
     DPU_ASSERT(dpu_get_nr_dpus(dpu_set, &nr_of_dpus));
-    printf("Allocated %d DPU(s)\n", nr_of_dpus);
-    unsigned int i = 0;
+    assert(nr_of_dpus == NR_DPUS);
+    timer.time[0] = 0; // alloc
+    timer.time[1] = 0; // load
+    timer.time[6] = 0; // free
+#endif
 
-    const unsigned int input_size = p.exp == 0 ? p.input_size * nr_of_dpus : p.input_size;
+    unsigned int i = 0;
+    const unsigned int input_size = p.exp == 0 ? p.input_size * NR_DPUS : p.input_size;
     const unsigned int input_size_8bytes = 
         ((input_size * sizeof(T)) % 8) != 0 ? roundup(input_size, 8) : input_size; // Input size per DPU (max.), 8-byte aligned
-    const unsigned int input_size_dpu = divceil(input_size, nr_of_dpus); // Input size per DPU (max.)
+    const unsigned int input_size_dpu = divceil(input_size, NR_DPUS); // Input size per DPU (max.)
     const unsigned int input_size_dpu_8bytes = 
         ((input_size_dpu * sizeof(T)) % 8) != 0 ? roundup(input_size_dpu, 8) : input_size_dpu; // Input size per DPU (max.), 8-byte aligned
 
     // Input/output allocation
-    A = malloc(input_size_dpu_8bytes * nr_of_dpus * sizeof(T));
-    B = malloc(input_size_dpu_8bytes * nr_of_dpus * sizeof(T));
-    C = malloc(input_size_dpu_8bytes * nr_of_dpus * sizeof(T));
-    C2 = malloc(input_size_dpu_8bytes * nr_of_dpus * sizeof(T));
+    A = malloc(input_size_dpu_8bytes * NR_DPUS * sizeof(T));
+    B = malloc(input_size_dpu_8bytes * NR_DPUS * sizeof(T));
+    C = malloc(input_size_dpu_8bytes * NR_DPUS * sizeof(T));
+    C2 = malloc(input_size_dpu_8bytes * NR_DPUS * sizeof(T));
     T *bufferA = A;
     T *bufferB = B;
     T *bufferC = C2;
@@ -96,15 +101,37 @@ int main(int argc, char **argv) {
     // Loop over main kernel
     for(int rep = 0; rep < p.n_warmup + p.n_reps; rep++) {
 
-        // Compute output on CPU (performance comparison and verification purposes)
-        if(rep >= p.n_warmup)
+#if WITH_ALLOC_OVERHEAD
+        if(rep >= p.n_warmup) {
             start(&timer, 0, 0);
-        vector_addition_host(C, A, B, input_size);
-        if(rep >= p.n_warmup)
+        }
+        DPU_ASSERT(dpu_alloc(NR_DPUS, NULL, &dpu_set));
+        if(rep >= p.n_warmup) {
             stop(&timer, 0);
-
-        if(rep >= p.n_warmup)
+        }
+        if(rep >= p.n_warmup) {
             start(&timer, 1, 0);
+        }
+        DPU_ASSERT(dpu_load(dpu_set, DPU_BINARY, NULL));
+        if(rep >= p.n_warmup) {
+            stop(&timer, 1);
+        }
+        DPU_ASSERT(dpu_get_nr_dpus(dpu_set, &nr_of_dpus));
+        assert(nr_of_dpus == NR_DPUS);
+#endif
+
+        // Compute output on CPU (performance comparison and verification purposes)
+        if(rep >= p.n_warmup) {
+            start(&timer, 2, 0);
+        }
+        vector_addition_host(C, A, B, input_size);
+        if(rep >= p.n_warmup) {
+            stop(&timer, 2);
+        }
+
+        if(rep >= p.n_warmup) {
+            start(&timer, 3, 0);
+        }
         // Input arguments
         unsigned int kernel = 0;
         dpu_arguments_t input_arguments[NR_DPUS];
@@ -133,19 +160,20 @@ int main(int argc, char **argv) {
             DPU_ASSERT(dpu_prepare_xfer(dpu, bufferB + input_size_dpu_8bytes * i));
         }
         DPU_ASSERT(dpu_push_xfer(dpu_set, DPU_XFER_TO_DPU, DPU_MRAM_HEAP_POINTER_NAME, input_size_dpu_8bytes * sizeof(T), input_size_dpu_8bytes * sizeof(T), DPU_XFER_DEFAULT));
-        if(rep >= p.n_warmup)
-            stop(&timer, 1);
+        if(rep >= p.n_warmup) {
+            stop(&timer, 3);
+        }
 
         // Run DPU kernel
         if(rep >= p.n_warmup) {
-            start(&timer, 2, 0);
+            start(&timer, 4, 0);
             #if ENERGY
             DPU_ASSERT(dpu_probe_start(&probe));
             #endif
         }
         DPU_ASSERT(dpu_launch(dpu_set, DPU_SYNCHRONOUS));
         if(rep >= p.n_warmup) {
-            stop(&timer, 2);
+            stop(&timer, 4);
             #if ENERGY
             DPU_ASSERT(dpu_probe_stop(&probe));
             #endif
@@ -163,16 +191,28 @@ int main(int argc, char **argv) {
         }
 #endif
 
-        if(rep >= p.n_warmup)
-            start(&timer, 3, 0);
+        if(rep >= p.n_warmup) {
+            start(&timer, 5, 0);
+        }
         i = 0;
         // PARALLEL RETRIEVE TRANSFER
         DPU_FOREACH(dpu_set, dpu, i) {
             DPU_ASSERT(dpu_prepare_xfer(dpu, bufferC + input_size_dpu_8bytes * i));
         }
         DPU_ASSERT(dpu_push_xfer(dpu_set, DPU_XFER_FROM_DPU, DPU_MRAM_HEAP_POINTER_NAME, input_size_dpu_8bytes * sizeof(T), input_size_dpu_8bytes * sizeof(T), DPU_XFER_DEFAULT));
-        if(rep >= p.n_warmup)
-            stop(&timer, 3);
+        if(rep >= p.n_warmup) {
+            stop(&timer, 5);
+        }
+
+#if WITH_ALLOC_OVERHEAD
+        if(rep >= p.n_warmup) {
+            start(&timer, 6, 0);
+        }
+        DPU_ASSERT(dpu_free(dpu_set));
+        if(rep >= p.n_warmup) {
+            stop(&timer, 6);
+        }
+#endif
 
         // Check output
         bool status = true;
@@ -187,17 +227,24 @@ int main(int argc, char **argv) {
         if (status) {
             printf("[" ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "] Outputs are equal\n");
             if (rep >= p.n_warmup) {
-                printf("[::] VA NMC | n_dpus=%d n_tasklets=%d e_type=%s block_size_B=%d n_elements=%d "
-                    "| throughput_cpu_MBps=%f throughput_pim_MBps=%f throughput_MBps=%f",
-                    nr_of_dpus, NR_TASKLETS, XSTR(T), BLOCK_SIZE, input_size,
-                    input_size * 3 * sizeof(T) / timer.time[0],
-                    input_size * 3 * sizeof(T) / (timer.time[2]),
-                    input_size * 3 * sizeof(T) / (timer.time[1] + timer.time[2] + timer.time[3]));
-                printf(" throughput_cpu_MOpps=%f throughput_pim_MOpps=%f throughput_MOpps=%f",
-                    input_size / timer.time[0],
-                    input_size / (timer.time[2]),
-                    input_size / (timer.time[1] + timer.time[2] + timer.time[3]));
-                printall(&timer, 3);
+                printf("[::] VA UPMEM | n_dpus=%d n_tasklets=%d e_type=%s block_size_B=%d n_elements=%d ",
+                    nr_of_dpus, NR_TASKLETS, XSTR(T), BLOCK_SIZE, input_size);
+                printf("| latency_alloc_us=%f latency_load_us=%f latency_cpu_us=%f latency_write_us=%f latency_kernel_us=%f latency_read_us=%f latency_free_us=%f",
+                    timer.time[0],
+                    timer.time[1],
+                    timer.time[2],
+                    timer.time[3],
+                    timer.time[4],
+                    timer.time[5],
+                    timer.time[6]);
+                printf(" throughput_cpu_MBps=%f throughput_upmem_kernel_MBps=%f throughput_upmem_total_MBps=%f",
+                    input_size * 3 * sizeof(T) / timer.time[2],
+                    input_size * 3 * sizeof(T) / (timer.time[4]),
+                    input_size * 3 * sizeof(T) / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5] + timer.time[6]));
+                printf(" throughput_cpu_MOpps=%f throughput_upmem_kernel_MOpps=%f throughput_upmem_total_MOpps=%f\n",
+                    input_size / timer.time[2],
+                    input_size / (timer.time[4]),
+                    input_size / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5] + timer.time[6]));
             }
         } else {
             printf("[" ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "] Outputs differ!\n");
@@ -228,7 +275,10 @@ int main(int argc, char **argv) {
     free(B);
     free(C);
     free(C2);
+
+#if !WITH_ALLOC_OVERHEAD
     DPU_ASSERT(dpu_free(dpu_set));
+#endif
 	
     return 0;
 }
--- VA/run.sh
+++ VA/run.sh
@@ -8,19 +8,22 @@ set -e
 # -e: number of timed iterations
 # -i: input size (number of elements, not number of bytes!)
 
+(
+
 echo "prim-benchmarks VA (dfatool edition)"
 echo "Started at $(date)"
 echo "Revision $(git describe --always)"
 
-for nr_dpus in 1 2 4 8 16 32 64 128 256 512; do
-	for nr_tasklets in 1 2 3 4 6 8 10 12 16 20 24; do
-		for i in  2048 4096 8192 16384 65536 262144 1048576 2621440; do
+for nr_dpus in 1 4 8 16 32 64 128 256 512 768 1024 1536 2048; do
+	for nr_tasklets in 8 12 16 20 24; do
+		for i in  2048 4096 8192 16384 65536 262144 1048576 2621440 167772160; do
 			for dt in CHAR SHORT INT32 INT64 FLOAT DOUBLE; do
 				echo
-				if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10 TYPE=${dt}; then
-					bin/host_code -w 0 -e 100 -i ${i} || true
+				if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10 TYPE=${dt} WITH_ALLOC_OVERHEAD=1; then
+					timeout --foreground -k 1m 30m bin/host_code -w 0 -e 100 -i ${i} -x 1 || true
 				fi
 			done
 		done
 	done
 done
+) | tee "log-$(hostname).txt"
