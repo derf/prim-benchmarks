3cd15f8dc1abab7e89615740688418751c4d57a0
Birte Kristina Friesel <birte.friesel@uos.de>
2024-03-13 09:39:03 +0100

    BS: throughput is based on num queries, not input size

--- BS/host/app.c
+++ BS/host/app.c
@@ -241,10 +241,10 @@ int main(int argc, char **argv) {
 		#endif
 
 		// Retrieve results
+		dpu_results_t* results_retrieve[NR_DPUS];
 		if (rep >= p.n_warmup) {
 			start(&timer, 5, 0);
 		}
-		dpu_results_t* results_retrieve[NR_DPUS];
 		i = 0;
 		DPU_FOREACH(dpu_set, dpu, i)
 		{
@@ -300,21 +300,21 @@ int main(int argc, char **argv) {
 					timer.time[5],
 					timer.time[6]);
 				printf(" throughput_cpu_MBps=%f throughput_upmem_kernel_MBps=%f throughput_upmem_total_MBps=%f",
-					input_size * sizeof(DTYPE) / timer.time[2],
-					input_size * sizeof(DTYPE) / (timer.time[4]),
-					input_size * sizeof(DTYPE) / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5] + timer.time[6]));
+					num_querys * sizeof(DTYPE) / timer.time[2],
+					num_querys * sizeof(DTYPE) / (timer.time[4]),
+					num_querys * sizeof(DTYPE) / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5] + timer.time[6]));
 				printf(" throughput_upmem_wxr_MBps=%f throughput_upmem_lwxr_MBps=%f throughput_upmem_alwxr_MBps=%f",
-					input_size * sizeof(DTYPE) / (timer.time[3] + timer.time[4] + timer.time[5]),
-					input_size * sizeof(DTYPE) / (timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5]),
-					input_size * sizeof(DTYPE) / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5]));
+					num_querys * sizeof(DTYPE) / (timer.time[3] + timer.time[4] + timer.time[5]),
+					num_querys * sizeof(DTYPE) / (timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5]),
+					num_querys * sizeof(DTYPE) / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5]));
 				printf(" throughput_cpu_MOpps=%f throughput_upmem_kernel_MOpps=%f throughput_upmem_total_MOpps=%f",
-					input_size / timer.time[2],
-					input_size / (timer.time[4]),
-					input_size / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5] + timer.time[6]));
+					num_querys / timer.time[2],
+					num_querys / (timer.time[4]),
+					num_querys / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5] + timer.time[6]));
 				printf(" throughput_upmem_wxr_MOpps=%f throughput_upmem_lwxr_MOpps=%f throughput_upmem_alwxr_MOpps=%f\n",
-					input_size / (timer.time[3] + timer.time[4] + timer.time[5]),
-					input_size / (timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5]),
-					input_size / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5]));
+					num_querys / (timer.time[3] + timer.time[4] + timer.time[5]),
+					num_querys / (timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5]),
+					num_querys / (timer.time[0] + timer.time[1] + timer.time[3] + timer.time[4] + timer.time[5]));
 			}
 		} else {
 			printf("[" ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET "] results differ!\n");
