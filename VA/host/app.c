d3e350249caeb28882fefd6e2cc0676e2f2c8808
Birte Kristina Friesel <birte.friesel@uos.de>
2024-07-04 14:51:35 +0200

    VA: Remove legacy SDK_SINGLETHREADED macro

--- VA/host/app.c
+++ VA/host/app.c
@@ -34,12 +34,6 @@
 #include <dpu_target_macros.h>
 #endif
 
-#if SDK_SINGLETHREADED
-#define DPU_ALLOC_PROFILE "nrThreadsPerRank=0"
-#else
-#define DPU_ALLOC_PROFILE NULL
-#endif
-
 // Pointer declaration
 static T* A;
 static T* B;
@@ -81,7 +75,7 @@ int main(int argc, char **argv) {
 
     // Allocate DPUs and load binary
 #if !WITH_ALLOC_OVERHEAD
-    DPU_ASSERT(dpu_alloc(NR_DPUS, DPU_ALLOC_PROFILE, &dpu_set));
+    DPU_ASSERT(dpu_alloc(NR_DPUS, NULL, &dpu_set));
     timer.time[0] = 0; // alloc
 #endif
 #if !WITH_LOAD_OVERHEAD
@@ -122,7 +116,7 @@ int main(int argc, char **argv) {
         if(rep >= p.n_warmup) {
             start(&timer, 0, 0);
         }
-        DPU_ASSERT(dpu_alloc(NR_DPUS, DPU_ALLOC_PROFILE, &dpu_set));
+        DPU_ASSERT(dpu_alloc(NR_DPUS, NULL, &dpu_set));
         if(rep >= p.n_warmup) {
             stop(&timer, 0);
         }
@@ -263,8 +257,8 @@ int main(int argc, char **argv) {
             if (rep >= p.n_warmup) {
                 printf("[::] VA UPMEM | n_dpus=%d n_ranks=%d n_tasklets=%d e_type=%s block_size_B=%d n_elements=%d n_elements_per_dpu=%d",
                     nr_of_dpus, nr_of_ranks, NR_TASKLETS, XSTR(T), BLOCK_SIZE, input_size, input_size / NR_DPUS);
-                printf(" b_sdk_singlethreaded=%d b_with_alloc_overhead=%d b_with_load_overhead=%d b_with_free_overhead=%d ",
-                    SDK_SINGLETHREADED, WITH_ALLOC_OVERHEAD, WITH_LOAD_OVERHEAD, WITH_FREE_OVERHEAD);
+                printf(" b_with_alloc_overhead=%d b_with_load_overhead=%d b_with_free_overhead=%d ",
+                    WITH_ALLOC_OVERHEAD, WITH_LOAD_OVERHEAD, WITH_FREE_OVERHEAD);
                 printf("| latency_alloc_us=%f latency_load_us=%f latency_cpu_us=%f latency_write_us=%f latency_kernel_us=%f latency_read_us=%f latency_free_us=%f",
                     timer.time[0],
                     timer.time[1],
