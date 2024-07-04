207a5fcf7f2d482f117a3d4a54484daf1fae92e2
Birte Kristina Friesel <birte.friesel@uos.de>
2024-07-04 15:46:18 +0200

    BS: Add separate files for HBM and NMC benchmarks

---
+++ BS/dimes-hetsim-hbm.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+cd baselines/cpu
+make -B NUMA=1
+
+mkdir -p log/$(hostname)
+fn=log/$(hostname)/$(date +%Y%m%d)
+
+# upstream DPU version uses 2048576 * uint64 ≈ 16 MiB (DPU max: 64 MiB)
+# upstream DPU version uses 2 queries
+# * uint64 == 16 MiB -- fits into cache
+input_size_dpu=$(perl -E 'say 2 ** 21')
+# * uint64 == 8 MiB
+num_queries_dpu=1048576
+
+# * uint64 == 4 GiB
+input_size_hbm=$(perl -E 'say 2 ** 29')
+# * uint64 == 128 MiB
+num_queries_hbm=16777216
+
+run_benchmark() {
+	local "$@"
+	OMP_NUM_THREADS=${nr_threads} ./bs_omp ${input_size} ${num_queries} $ram $cpu
+	return $?
+}
+
+export -f run_benchmark
+
+(
+
+parallel -j1 --eta --joblog ${fn}.1.joblog --resume --header : \
+	run_benchmark i={i} nr_threads={nr_threads} ram={ram} cpu={cpu} \
+	input_size=${input_size_dpu} num_queries=${num_queries_dpu} \
+	::: i $(seq 1 20) \
+	::: nr_threads 1 2 4 8 12 16 \
+	::: cpu $(seq 0 7) \
+	::: ram $(seq 0 15)
+
+parallel -j1 --eta --joblog ${fn}.2.joblog --resume --header : \
+	run_benchmark i={i} nr_threads={nr_threads} ram={ram} cpu={cpu} \
+	input_size=${input_size_hbm} num_queries=${num_queries_hbm} \
+	::: i $(seq 1 20) \
+	::: nr_threads 1 2 4 8 12 16 \
+	::: cpu $(seq 0 7) \
+	::: ram $(seq 0 15)
+
+parallel -j1 --eta --joblog ${fn}.3.joblog --resume --header : \
+	run_benchmark i={i} nr_threads={nr_threads} ram={ram} cpu={cpu} \
+	input_size=${input_size_dpu} num_queries=${num_queries_dpu} \
+	::: i $(seq 1 20) \
+	::: nr_threads 32 48 64 96 128 \
+	::: cpu -1 \
+	::: ram $(seq 0 15)
+
+parallel -j1 --eta --joblog ${fn}.4.joblog --resume --header : \
+	run_benchmark i={i} nr_threads={nr_threads} ram={ram} cpu={cpu} \
+	input_size=${input_size_hbm} num_queries=${num_queries_hbm} \
+	::: i $(seq 1 20) \
+	::: nr_threads 32 48 64 96 128 \
+	::: cpu -1 \
+	::: ram $(seq 0 15)
+
+) > ${fn}.txt
+
+xz -f -v -9 -M 800M ${fn}.txt
---
+++ BS/dimes-hetsim-nmc.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+mkdir -p log/$(hostname) baselines/cpu/log/$(hostname)
+fn=log/$(hostname)/$(date +%Y%m%d)
+
+# upstream DPU version uses 2048576 * uint64 ≈ 16 MiB (DPU max: 64 MiB)
+# upstream DPU version uses 2 queries
+input_size_upstream=2048576
+num_queries_upstream=2
+# here: 32 MiB and 1048576 queries
+input_size_dpu=$(perl -E 'say 2 ** 22')
+num_queries_dpu=1048576
+
+run_benchmark_nmc() {
+	local "$@"
+	if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} WITH_ALLOC_OVERHEAD=1 WITH_LOAD_OVERHEAD=1 WITH_FREE_OVERHEAD=1 PROBLEM_SIZE=${num_queries}; then
+		bin/bs_host -w 0 -e 100 -i ${input_size}
+	fi
+	return $?
+}
+
+export -f run_benchmark_nmc
+
+run_benchmark_baseline() {
+	local "$@"
+	OMP_NUM_THREADS=${nr_threads} ./bs_omp ${input_size} ${num_queries} ${ram} ${cpu}
+	return $?
+}
+
+export -f run_benchmark_baseline
+
+(
+
+parallel -j1 --eta --joblog ${fn}.1.joblog --resume --header : \
+	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 \
+	num_queries=${num_queries_upstream} input_size=${input_size_upstream} \
+	::: nr_dpus 64 128 256 512 768 1024 1536 2048 2304
+
+parallel -j1 --eta --joblog ${fn}.2.joblog --resume --header : \
+	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 \
+	num_queries=${num_queries_dpu} input_size=${input_size_dpu} \
+	::: nr_dpus 64 128 256 512 768 1024 1536 2048 2304
+
+) > ${fn}.txt
+
+xz -f -v -9 -M 800M ${fn}.txt
+
+cd baselines/cpu
+make -B NUMA=1
+
+(
+
+parallel -j1 --eta --joblog ${fn}.1.joblog --resume --header : \
+	run_benchmark_baseline i={i} nr_threads={nr_threads} ram={ram} cpu={cpu} \
+	num_queries=${num_queries_upstream} input_size=${input_size_upstream} \
+	::: i $(seq 1 20) \
+	::: cpu 0 1 \
+	::: ram 0 1 \
+	::: nr_threads 1 2 4 8 12 16 32
+
+parallel -j1 --eta --joblog ${fn}.2.joblog --resume --header : \
+	run_benchmark_baseline i={i} nr_threads={nr_threads} ram={ram} cpu={cpu} \
+	num_queries=${num_queries_dpu} input_size=${input_size_dpu} \
+	::: i $(seq 1 20) \
+	::: cpu 0 1 \
+	::: ram 0 1 \
+	::: nr_threads 1 2 4 8 12 16 32
+
+parallel -j1 --eta --joblog ${fn}.3.joblog --resume --header : \
+	run_benchmark_baseline i={i} nr_threads={nr_threads} ram={ram} cpu={cpu} \
+	num_queries=${num_queries_upstream} input_size=${input_size_upstream} \
+	::: i $(seq 1 20) \
+	::: cpu -1 \
+	::: ram any \
+	::: nr_threads 48 64
+
+parallel -j1 --eta --joblog ${fn}.4.joblog --resume --header : \
+	run_benchmark_baseline i={i} nr_threads={nr_threads} ram={ram} cpu={cpu} \
+	num_queries=${num_queries_dpu} input_size=${input_size_dpu} \
+	::: i $(seq 1 20) \
+	::: cpu -1 \
+	::: ram any \
+	::: nr_threads 48 64
+
+) > ${fn}.txt
+
+xz -f -v -9 -M 800M ${fn}.txt
