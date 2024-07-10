4a246d3ce68fdeeee358ad1ea855291dd095bb19
Birte Kristina Friesel <birte.friesel@uos.de>
2024-07-10 16:28:57 +0200

    VA: nits

--- VA/dimes-hetsim-nmc.sh
+++ VA/dimes-hetsim-nmc.sh
@@ -20,7 +20,7 @@ export -f run_benchmark_nmc
 
 echo "NMC single-node operation (1/2)" >&2
 
-parallel -j1 --eta --joblog ${fn}.1.joblog --resume --header : \
+parallel -j1 --eta --joblog ${fn}.1.joblog --header : \
 	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 input_size={input_size} numa_rank={numa_rank} \
 	::: numa_rank 0 1 \
 	::: nr_dpus 64 128 256 512 768 1024 \
@@ -28,9 +28,9 @@ parallel -j1 --eta --joblog ${fn}.1.joblog --resume --header : \
 
 echo "NMC multi-node operation (2/2)" >&2
 
-parallel -j1 --eta --joblog ${fn}.2.joblog --resume --header : \
+parallel -j1 --eta --joblog ${fn}.2.joblog --header : \
 	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 input_size={input_size} numa_rank={numa_rank} \
-	::: numa_rank -1 \
+	::: numa_rank any \
 	::: nr_dpus 1536 2048 2304 \
 	::: input_size 167772160
 
@@ -45,20 +45,20 @@ make -B NUMA=1
 
 echo "CPU single-node operation (1/2)" >&2
 
-parallel -j1 --eta --joblog ${fn}.1.joblog --resume --header : \
+parallel -j1 --eta --joblog ${fn}.1.joblog --header : \
 	./va -i {input_size} -a {ram} -b {ram} -c {cpu} -t {nr_threads} -w 0 -e 40 \
 	::: ram 0 1 \
 	::: cpu 0 1 \
-	::: nr_threads 1 2 4 8 12 16 32 \
+	::: nr_threads 1 2 4 8 12 16 \
 	::: input_size 167772160
 
 echo "CPU multi-node operation (1/2)" >&2
 
-parallel -j1 --eta --joblog ${fn}.2.joblog --resume --header : \
+parallel -j1 --eta --joblog ${fn}.2.joblog --header : \
 	./va -i {input_size} -a {ram} -b {ram} -c {cpu} -t {nr_threads} -w 0 -e 40 \
-	::: ram any \
+	::: ram 0 1 \
 	::: cpu -1 \
-	::: nr_threads 48 64 \
+	::: nr_threads 24 32 \
 	::: input_size 167772160
 
 ) > ${fn}.txt
