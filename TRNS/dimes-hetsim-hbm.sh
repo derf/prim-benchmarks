d1047cc7d6c6b9a24ba8df48782ae7aa0bd92949
Birte Kristina Friesel <birte.friesel@uos.de>
2024-07-17 09:41:57 +0200

    TRNS: document and update dimes-hetsim scripts

--- TRNS/dimes-hetsim-hbm.sh
+++ TRNS/dimes-hetsim-hbm.sh
@@ -6,19 +6,38 @@ make -B NUMA=1
 mkdir -p log/$(hostname)
 fn=log/$(hostname)/$(date +%Y%m%d)
 
-# DPU version: -m m -n n -o M_ -p N_
-# input size: sizeof(T) * M_ * m * N_ * n
+# Args: -m m -n n -o M_ -p N_
+#
+# Input: (M_ * m) × (N_ * n) matrix
+# Output: (N_* n) × (M_ * m) matrix
+# Step 1: transpose (M_ * m) × N_ matrix that consits of tiles of size n
+#   CPU version: explicit
+#   DPU version: implicit (appropriate write operations to DPUs)
+# Step 2: transpose m × n matrix; this happens N_ * M_ times.
+#   DPU version: Each tasklet transposes a single m × n matrix / tile.
+#   (16 × 8 tile takes up 1 KiB WRAM)
+# Step 3: Transpose M_ × n matrix that consists of tiles of size m.
+#
+# Note for DPU version: if M_ > #DPUs, steps 1 through 3 are repeated.
+# Number of repetitions == ceil(M_ / #DPUS)
+# For Hetsim benchmarks, we set M_ == #DPUs to simplify the task graph (no repetitions that depend on the number of available DPUs).
+# Just in case, there is also a configuration with M_ == 2048 independent of #DPUs
+#
+# input size: uint64(DPU)/double(CPU) * M_ * m * N_ * n
+# output size: uint64(DPU)/double(CPU) * M_ * m * N_ * n -- on DPU only; CPU version operates in-place
 # Upstream DPU version uses int64_t, -p 2048 -o 12288 -x 1 [implicit -m 16 -n 8]
 # Upstream CPU version uses double,  -p 2556 -o 4096 -m 16 -n 8 and fails with -o 12288 (allocation error)
 #
-# Here: -p 2048 -o 2048 -m 16 -n 8 for both
+# -p         2048 -o 2048 -m 16 -n 8 -> matrix size: 4 GiB
+# -p [64 .. 2304] -o 2048 -m 16 -n 8 -> matrix size: 128 MiB .. 4.5 GiB
 
 (
 
 echo "CPU single-node operation (1/2)" >&2
 
 parallel -j1 --eta --joblog ${fn}.1.joblog --header : \
-	./trns -w 0 -r 40 -p 2048 -o 2048 -m 16 -n 8 -t {nr_threads} -a {ram} -b {ram} -c {cpu} \
+	./trns -w 0 -r 5 -p {p} -o 2048 -m 16 -n 8 -t {nr_threads} -a {ram} -b {ram} -c {cpu} \
+	::: p 64 128 256 512 768 1024 1536 2048 2304 \
 	::: ram $(seq 0 15) \
 	::: cpu $(seq 0 7) \
 	::: nr_threads 1 2 4 8 12 16
@@ -26,7 +45,8 @@ parallel -j1 --eta --joblog ${fn}.1.joblog --header : \
 echo "CPU multi-node operation (1/2)" >&2
 
 parallel -j1 --eta --joblog ${fn}.2.joblog --header : \
-	./trns -w 0 -r 40 -p 2048 -o 2048 -m 16 -n 8 -t {nr_threads} -a {ram} -b {ram} -c {cpu} \
+	./trns -w 0 -r 40 -p {p} -o 2048 -m 16 -n 8 -t {nr_threads} -a {ram} -b {ram} -c {cpu} \
+	::: p 64 128 256 512 768 1024 1536 2048 2304 \
 	::: ram $(seq 0 15) \
 	::: cpu -1 \
 	::: nr_threads 32 48 64 96 128
--- TRNS/dimes-hetsim-nmc.sh
+++ TRNS/dimes-hetsim-nmc.sh
@@ -3,20 +3,36 @@
 mkdir -p log/$(hostname) baselines/cpu/log/$(hostname)
 fn=log/$(hostname)/$(date +%Y%m%d)
 
-# DPU version: -m m -n n -o M_ -p N_
-# input size: sizeof(T) * M_ * m * N_ * n
+# Args: -m m -n n -o M_ -p N_
+#
+# Input: (M_ * m) × (N_ * n) matrix
+# Output: (N_* n) × (M_ * m) matrix
+# Step 1: transpose (M_ * m) × N_ matrix that consits of tiles of size n
+#   CPU version: explicit
+#   DPU version: implicit (appropriate write operations to DPUs)
+# Step 2: transpose m × n matrix; this happens N_ * M_ times.
+#   DPU version: Each tasklet transposes a single m × n matrix / tile.
+#   (16 × 8 tile takes up 1 KiB WRAM)
+# Step 3: Transpose M_ × n matrix that consists of tiles of size m.
+#
+# Note for DPU version: if M_ > #DPUs, steps 1 through 3 are repeated.
+# Number of repetitions == ceil(M_ / #DPUS)
+# For Hetsim benchmarks, we set M_ == #DPUs to simplify the task graph (no repetitions that depend on the number of available DPUs).
+# Just in case, there is also a configuration with M_ == 2048 independent of #DPUs
+#
+# input size: uint64(DPU)/double(CPU) * M_ * m * N_ * n
+# output size: uint64(DPU)/double(CPU) * M_ * m * N_ * n -- on DPU only; CPU version operates in-place
 # Upstream DPU version uses int64_t, -p 2048 -o 12288 -x 1 [implicit -m 16 -n 8]
 # Upstream CPU version uses double,  -p 2556 -o 4096 -m 16 -n 8 and fails with -o 12288 (allocation error)
 #
-# Here: -p 2048 -o 2048 -m 16 -n 8 for both
-
-# upstream uses 167772160 * 2 * int32 == 2.5 GiB input and 1.25 GiB output for DPU version
+# -p         2048 -o 2048 -m 16 -n 8 -> matrix size: 4 GiB
+# -p [64 .. 2304] -o 2048 -m 16 -n 8 -> matrix size: 128 MiB .. 4.5 GiB
 
 run_benchmark_nmc() {
 	local "$@"
 	sudo limit_ranks_to_numa_node ${numa_rank}
 	if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} WITH_ALLOC_OVERHEAD=1 WITH_LOAD_OVERHEAD=1 WITH_FREE_OVERHEAD=1; then
-		bin/host_code -w 0 -e 100 -p 2048 -o 2048 -m 16 -n 8 -x 1
+		bin/host_code -w 0 -e 40 -p ${p} -o 2048 -m 16 -n 8 -x 1
 	fi
 	return $?
 }
@@ -25,17 +41,31 @@ export -f run_benchmark_nmc
 
 (
 
-echo "NMC single-node operation (1/2)" >&2
+echo "NMC single-node operation (1/4)" >&2
 
 parallel -j1 --eta --joblog ${fn}.1.joblog --header : \
-	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 input_size={input_size} numa_rank={numa_rank} \
+	run_benchmark_nmc p={nr_dpus} nr_dpus={nr_dpus} nr_tasklets=16 input_size={input_size} numa_rank={numa_rank} \
 	::: numa_rank 0 1 \
 	::: nr_dpus 64 128 256 512 768 1024
 
-echo "NMC multi-node operation (2/2)" >&2
+echo "NMC multi-node operation (2/4)" >&2
 
 parallel -j1 --eta --joblog ${fn}.2.joblog --header : \
-	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 input_size={input_size} numa_rank={numa_rank} \
+	run_benchmark_nmc p={nr_dpus} nr_dpus={nr_dpus} nr_tasklets=16 input_size={input_size} numa_rank={numa_rank} \
+	::: numa_rank any \
+	::: nr_dpus 1536 2048 2304
+
+echo "NMC single-node operation (3/4)" >&2
+
+parallel -j1 --eta --joblog ${fn}.3.joblog --header : \
+	run_benchmark_nmc p=2048 nr_dpus={nr_dpus} nr_tasklets=16 input_size={input_size} numa_rank={numa_rank} \
+	::: numa_rank 0 1 \
+	::: nr_dpus 64 128 256 512 768 1024
+
+echo "NMC multi-node operation (4/4)" >&2
+
+parallel -j1 --eta --joblog ${fn}.4.joblog --header : \
+	run_benchmark_nmc p=2048 nr_dpus={nr_dpus} nr_tasklets=16 input_size={input_size} numa_rank={numa_rank} \
 	::: numa_rank any \
 	::: nr_dpus 1536 2048 2304
 
@@ -51,18 +81,20 @@ make -B NUMA=1
 echo "CPU single-node operation (1/2)" >&2
 
 parallel -j1 --eta --joblog ${fn}.1.joblog --header : \
-	./trns -w 0 -r 40 -p 2048 -o 2048 -m 16 -n 8 -t {nr_threads} \
+	./trns -w 0 -r 40 -p {p} -o 2048 -m 16 -n 8 -t {nr_threads} -a {ram} -b {ram} -c {cpu} \
+	::: p 64 128 256 512 768 1024 1536 2048 2304 \
 	::: ram 0 1 \
 	::: cpu 0 1 \
-	::: nr_threads 1 2 4 8 12 16 32
+	::: nr_threads 1 2 4 8 12 16
 
-echo "CPU multi-node operation (1/2)" >&2
+echo "CPU multi-node operation (2/2)" >&2
 
 parallel -j1 --eta --joblog ${fn}.2.joblog --header : \
-	./trns -w 0 -r 40 -p 2048 -o 2048 -m 16 -n 8 -t {nr_threads} \
+	./trns -w 0 -r 40 -p {p} -o 2048 -m 16 -n 8 -t {nr_threads} -a {ram} -b {ram} -c {cpu} \
+	::: p 64 128 256 512 768 1024 1536 2048 2304 \
 	::: ram 0 1 \
 	::: cpu -1 \
-	::: nr_threads 48 64
+	::: nr_threads 24 32
 
 ) > ${fn}.txt
 
