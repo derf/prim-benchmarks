91491e329c1116a805229cfa75db0516a1268e54
Birte Kristina Friesel <derf@finalrewind.org>
2025-05-15 11:40:39 +0200

    GEMV: Fix host binary invocation

--- GEMV/benchmark-scripts/ccmcc25.sh
+++ GEMV/benchmark-scripts/ccmcc25.sh
@@ -11,12 +11,12 @@ run_benchmark_nmc() {
 	sudo limit_ranks_to_numa_node ${numa_rank}
 	make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} \
 		aspectc=1 aspectc_timing=1 dfatool_timing=0
-	bin/bs_host -w 0 -e 50 -n ${nr_cols} -m ${nr_rows} 2>&1
+	bin/gemv_host -w 0 -e 50 -n ${nr_cols} -m ${nr_rows} 2>&1
 }
 
 export -f run_benchmark_nmc
 
-echo "prim-benchmarks  BS  $(git describe --all --long)  $(git rev-parse HEAD)  $(date -R)" >> ${fn}.txt
+echo "prim-benchmarks  GEMV  $(git describe --all --long)  $(git rev-parse HEAD)  $(date -R)" >> ${fn}.txt
 
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
 	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 numa_rank=any nr_cols={nr_cols} nr_rows={nr_rows} \
