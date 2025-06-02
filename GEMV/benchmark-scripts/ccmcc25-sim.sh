496f4233a831b4378ef6ae29b1d273f8da8af186
Birte Kristina Friesel <birte.friesel@uos.de>
2025-06-02 15:48:05 +0200

    GEMV: port simulation benchmark script

--- GEMV/benchmark-scripts/ccmcc25-sim.sh
+++ GEMV/benchmark-scripts/ccmcc25-sim.sh
@@ -5,7 +5,6 @@ mkdir -p log/$(hostname)
 run_benchmark_nmc() {
 	local "$@"
 	set -e
-	sudo limit_ranks_to_numa_node ${numa_rank}
 	make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} \
 		aspectc=1 aspectc_timing=1 dfatool_timing=0
 	bin/gemv_host -w 0 -e 5 -n ${nr_cols} -m ${nr_rows} 2>&1
@@ -13,19 +12,15 @@ run_benchmark_nmc() {
 
 export -f run_benchmark_nmc
 
-for sdk in 2023.2.0 2024.1.0 2024.2.0 2025.1.0; do
+fn=log/$(hostname)/ccmcc25-sdk${sdk}-sim
 
-	fn=log/$(hostname)/ccmcc25-sdk${sdk}
+source ~/lib/local/upmem/upmem-2025.1.0-Linux-x86_64/upmem_env.sh simulator
 
-	source /opt/upmem/upmem-${sdk}-Linux-x86_64/upmem_env.sh
+echo "prim-benchmarks  GEMV  $(git describe --all --long)  $(git rev-parse HEAD)  $(date -R)" >> ${fn}.txt
 
-	echo "prim-benchmarks  GEMV  $(git describe --all --long)  $(git rev-parse HEAD)  $(date -R)" >> ${fn}.txt
-
-	parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
-		run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 numa_rank=any nr_cols={nr_cols} nr_rows={nr_rows} \
-		::: nr_dpus 64 128 256 512 768 1024 1536 2048 2304 \
-		::: nr_cols 2048 4096 8192 \
-		::: nr_rows 40960 81920 163840 \
-	>> ${fn}.txt
-
-done
+parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
+	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 nr_cols={nr_cols} nr_rows={nr_rows} \
+	::: nr_dpus 1 2 4 8 16 32 48 64 \
+	::: nr_cols 256 512 768 1024 1536 \
+	::: nr_rows 512 68 1024 1536 2048 \
+>> ${fn}.txt
