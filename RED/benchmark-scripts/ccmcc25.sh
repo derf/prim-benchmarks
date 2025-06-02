ecc41ac41f3e771d1bc591af9aab4cf4258abb83
Birte Kristina Friesel <birte.friesel@uos.de>
2025-06-02 15:50:56 +0200

    RED: benchmark script

--- RED/benchmark-scripts/ccmcc25.sh
+++ RED/benchmark-scripts/ccmcc25.sh
@@ -6,21 +6,25 @@ run_benchmark_nmc() {
 	local "$@"
 	set -e
 	sudo limit_ranks_to_numa_node ${numa_rank}
-	make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10 \
+	make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} \
 		aspectc=1 aspectc_timing=1 dfatool_timing=0
 	bin/host_code -w 0 -e 5 -i ${nr_elements} 2>&1
 }
 
 export -f run_benchmark_nmc
 
-fn=log/$(hostname)/ccmcc25-sdk${sdk}-sim
+for sdk in 2023.2.0 2024.1.0 2024.2.0 2025.1.0; do
 
-source ~/lib/local/upmem/upmem-2025.1.0-Linux-x86_64/upmem_env.sh simulator
+	fn=log/$(hostname)/ccmcc25-sdk${sdk}
 
-echo "prim-benchmarks  BS  $(git describe --all --long)  $(git rev-parse HEAD)  $(date -R)" >> ${fn}.txt
+	source /opt/upmem/upmem-${sdk}-Linux-x86_64/upmem_env.sh
 
-parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
-	run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 numa_rank=any nr_elements={nr_elements} \
-		::: nr_dpus 64 128 256 512 768 1024 1536 2048 2304 \
-		::: nr_elements $((2**27)) $((2**28)) $((2**29)) \
->> ${fn}.txt
+	echo "prim-benchmarks  RED  $(git describe --all --long)  $(git rev-parse HEAD)  $(date -R)" >> ${fn}.txt
+
+	parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
+		run_benchmark_nmc nr_dpus={nr_dpus} nr_tasklets=16 numa_rank=any nr_elements={nr_elements} \
+			::: nr_dpus 64 128 256 512 768 1024 1536 2048 2304 \
+			::: nr_elements $((2**27)) $((2**28)) $((2**29)) \
+	>> ${fn}.txt
+
+done
