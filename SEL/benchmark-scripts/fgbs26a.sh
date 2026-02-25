6c3f1a720b7e0657462a922fa9846f6dcd0091bf
Birte Kristina Friesel <birte.friesel@uos.de>
2026-02-25 08:30:37 +0100

    SEL NMC: Add fgbs26a benchmark script

---
+++ SEL/benchmark-scripts/fgbs26a.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p log/$(hostname)
+
+for sdk in 2023.2.0 2024.1.0 2024.2.0 2025.1.0; do
+
+	fn=log/$(hostname)/fgbs26a-sdk${sdk}.txt
+	: > ${fn}
+
+	source /opt/upmem/upmem-${sdk}-Linux-x86_64/upmem_env.sh
+
+	(
+		echo "prim-benchmarks SEL (dfatool fgbs26a edition)"
+		echo "Started at $(date)"
+		echo "Revision $(git describe --always)"
+
+		for nr_dpus in 2304 2048 2543; do
+			for nr_tasklets in 16; do
+				echo
+				if make -B NR_DPUS=${nr_dpus} NR_TASKLETS=${nr_tasklets} BL=10 WITH_ALLOC_OVERHEAD=1 WITH_LOAD_OVERHEAD=1 WITH_FREE_OVERHEAD=1; then
+					timeout --foreground -k 1m 30m bin/host_code -w 0 -e 100 -i $(( 2 ** 30 )) -x 1 || true
+				fi
+			done
+		done
+		echo "Completed at $(date)"
+	) | tee ${fn}
+
+done
