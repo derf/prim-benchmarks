44d65a79f6dbd9a89c31bff5a98654223d20a8af
Birte Kristina Friesel <birte.friesel@uos.de>
2026-06-12 13:55:29 +0200

    BS CPU: clang-format

--- BS/baselines/cpu/bs_omp.c
+++ BS/baselines/cpu/bs_omp.c
@@ -1,12 +1,12 @@
+#include <assert.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <assert.h>
 #include <time.h>
-#include <stdint.h>
+#include <unistd.h>
 
 #if WITH_BENCHMARK
 #include "timer.h"
@@ -23,19 +23,19 @@
 #endif
 
 #if NUMA
-#include <numaif.h>
 #include <numa.h>
+#include <numaif.h>
 
-void *mp_pages[1];
+void* mp_pages[1];
 int mp_status[1];
 int mp_nodes[1];
-struct bitmask *bitmask_in;
+struct bitmask* bitmask_in;
 int numa_node_in = -1;
 int numa_node_cpu = -1;
 #endif
 
 #if NUMA_MEMCPY
-struct bitmask *bitmask_cpu;
+struct bitmask* bitmask_cpu;
 int numa_node_cpu_memcpy = -1;
 int numa_node_local = -1;
 int numa_node_in_is_local = 0;
@@ -43,10 +43,10 @@ int numa_node_in_is_local = 0;
 
 #define DTYPE uint64_t
 /*
-* @brief creates a "test file" by filling a bufferwith values
-*/
-void create_test_file(DTYPE *input, uint64_t nr_elements, DTYPE *querys,
-		      uint64_t n_querys)
+ * @brief creates a "test file" by filling a bufferwith values
+ */
+void create_test_file(DTYPE* input, uint64_t nr_elements, DTYPE* querys,
+    uint64_t n_querys)
 {
 
 	srand(time(NULL));
@@ -62,16 +62,16 @@ void create_test_file(DTYPE *input, uint64_t nr_elements, DTYPE *querys,
 }
 
 /**
-* @brief compute output in the host
-*/
-uint64_t binarySearch(DTYPE *input, uint64_t input_size, DTYPE *querys,
-		      unsigned n_querys)
+ * @brief compute output in the host
+ */
+uint64_t binarySearch(DTYPE* input, uint64_t input_size, DTYPE* querys,
+    unsigned n_querys)
 {
 
 	uint64_t found = -1;
 	uint64_t q, r, l, m;
 
-#pragma omp parallel for private(q,r,l,m)
+#pragma omp parallel for private(q, r, l, m)
 	for (q = 0; q < n_querys; q++) {
 		l = 0;
 		r = input_size;
@@ -90,17 +90,16 @@ uint64_t binarySearch(DTYPE *input, uint64_t input_size, DTYPE *querys,
 			// If x is smaller, ignore right half
 			else
 				r = m - 1;
-
 		}
 	}
 
 	return found;
 }
 
-  /**
-  * @brief Main of the Host Application.
-  */
-int main(int argc, char **argv)
+/**
+ * @brief Main of the Host Application.
+ */
+int main(int argc, char** argv)
 {
 	(void)argc;
 #if WITH_BENCHMARK
@@ -124,11 +123,11 @@ int main(int argc, char **argv)
 		numa_set_membind(bitmask_in);
 		numa_free_nodemask(bitmask_in);
 	}
-	DTYPE *input = numa_alloc((input_size) * sizeof(DTYPE));
-	DTYPE *querys = numa_alloc((n_querys) * sizeof(DTYPE));
+	DTYPE* input = numa_alloc((input_size) * sizeof(DTYPE));
+	DTYPE* querys = numa_alloc((n_querys) * sizeof(DTYPE));
 #else
-	DTYPE *input = malloc((input_size) * sizeof(DTYPE));
-	DTYPE *querys = malloc((n_querys) * sizeof(DTYPE));
+	DTYPE* input = malloc((input_size) * sizeof(DTYPE));
+	DTYPE* querys = malloc((n_querys) * sizeof(DTYPE));
 #endif
 
 #if NUMA
@@ -138,11 +137,11 @@ int main(int argc, char **argv)
 		numa_free_nodemask(bitmask_cpu);
 	}
 #else
-	struct bitmask *bitmask_all = numa_allocate_nodemask();
+	struct bitmask* bitmask_all = numa_allocate_nodemask();
 	numa_bitmask_setall(bitmask_all);
 	numa_set_membind(bitmask_all);
 	numa_free_nodemask(bitmask_all);
-#endif				// NUMA_MEMCPY
+#endif // NUMA_MEMCPY
 #endif
 
 	DTYPE result_host = -1;
@@ -170,12 +169,13 @@ int main(int argc, char **argv)
 
 #if NUMA_MEMCPY
 	numa_node_in_is_local = ((numa_node_cpu == numa_node_in)
-				 || (numa_node_cpu + 8 == numa_node_in)) * 1;
+	                            || (numa_node_cpu + 8 == numa_node_in))
+	    * 1;
 #endif
 
 #if NUMA_MEMCPY
-	DTYPE *input_local = input;
-	DTYPE *querys_local = querys;
+	DTYPE* input_local = input;
+	DTYPE* querys_local = querys;
 	start(&timer, 1, 0);
 	if (!numa_node_in_is_local) {
 		input_local = numa_alloc((input_size) * sizeof(DTYPE));
@@ -217,15 +217,14 @@ int main(int argc, char **argv)
 
 #if NOP_SYNC
 	for (int rep = 0; rep < 200000; rep++) {
-		asm volatile ("nop"::);
+		asm volatile("nop" ::);
 	}
 #endif
 
 	perf_start();
 	start(&timer, 0, 0);
 #if NUMA_MEMCPY
-	result_host =
-	    binarySearch(input_local, input_size - 1, querys_local, n_querys);
+	result_host = binarySearch(input_local, input_size - 1, querys_local, n_querys);
 #else
 	result_host = binarySearch(input, input_size - 1, querys, n_querys);
 #endif
@@ -234,7 +233,7 @@ int main(int argc, char **argv)
 
 #if NOP_SYNC
 	for (int rep = 0; rep < 200000; rep++) {
-		asm volatile ("nop"::);
+		asm volatile("nop" ::);
 	}
 #endif
 
@@ -256,55 +255,52 @@ int main(int argc, char **argv)
 
 	if (status) {
 #if WITH_PERF
-			printf("[::] binarySearch | n_threads=%d e_type=%s n_elements=%lu"
+		printf("[::] binarySearch | n_threads=%d e_type=%s n_elements=%lu"
 #if NUMA
-					" numa_node_in=%d numa_node_out=%d numa_node_cpu=%d numa_distance_in_cpu=%d numa_distance_cpu_out=%d"
+		       " numa_node_in=%d numa_node_out=%d numa_node_cpu=%d numa_distance_in_cpu=%d numa_distance_cpu_out=%d"
 #endif
-					" |",
-				nr_threads, "uint64_t", input_size
+		       " |",
+		    nr_threads, "uint64_t", input_size
 #if NUMA
-				,
-				numa_node_in, numa_node_out, numa_node_cpu,
-				numa_distance(numa_node_in, numa_node_cpu),
-				numa_distance(numa_node_cpu, numa_node_out)
+		    ,
+		    numa_node_in, numa_node_out, numa_node_cpu,
+		    numa_distance(numa_node_in, numa_node_cpu),
+		    numa_distance(numa_node_cpu, numa_node_out)
 #endif
-			);
+		);
 		perf_print();
 #else
 #if NUMA_MEMCPY
-		printf
-		    ("[::] BS-CPU-MEMCPY | n_threads=%d e_type=%s n_elements=%lu"
-		     " numa_node_in=%d numa_node_local=%d numa_node_cpu=%d numa_node_cpu_memcpy=%d numa_distance_in_cpu=%d"
-		     " | throughput_MBps=%f throughput_MOpps=%f"
-		     " latency_kernel_us=%f latency_alloc_us=%f latency_memcpy_us=%f latency_free_us=%f latency_total_us=%f\n",
-		     nr_threads, "uint64_t", input_size, numa_node_in,
-		     numa_node_local, numa_node_cpu, numa_node_cpu_memcpy,
-		     numa_distance(numa_node_in, numa_node_cpu),
-		     n_querys * sizeof(DTYPE) / timer.time[0],
-		     n_querys / timer.time[0], timer.time[0], timer.time[1],
-		     timer.time[2], timer.time[3],
-		     timer.time[0] + timer.time[1] + timer.time[2] +
-		     timer.time[3]);
+		printf("[::] BS-CPU-MEMCPY | n_threads=%d e_type=%s n_elements=%lu"
+		       " numa_node_in=%d numa_node_local=%d numa_node_cpu=%d numa_node_cpu_memcpy=%d numa_distance_in_cpu=%d"
+		       " | throughput_MBps=%f throughput_MOpps=%f"
+		       " latency_kernel_us=%f latency_alloc_us=%f latency_memcpy_us=%f latency_free_us=%f latency_total_us=%f\n",
+		    nr_threads, "uint64_t", input_size, numa_node_in,
+		    numa_node_local, numa_node_cpu, numa_node_cpu_memcpy,
+		    numa_distance(numa_node_in, numa_node_cpu),
+		    n_querys * sizeof(DTYPE) / timer.time[0],
+		    n_querys / timer.time[0], timer.time[0], timer.time[1],
+		    timer.time[2], timer.time[3],
+		    timer.time[0] + timer.time[1] + timer.time[2] + timer.time[3]);
 #else
 		printf("[::] BS-CPU | n_threads=%d e_type=%s n_elements=%lu"
 #if NUMA
 		       " numa_node_in=%d numa_node_cpu=%d numa_distance_in_cpu=%d"
 #endif
 		       " | throughput_MBps=%f",
-		       nr_threads, "uint64_t", input_size,
+		    nr_threads, "uint64_t", input_size,
 #if NUMA
-		       numa_node_in, numa_node_cpu, numa_distance(numa_node_in,
-								  numa_node_cpu),
+		    numa_node_in, numa_node_cpu, numa_distance(numa_node_in, numa_node_cpu),
 #endif
-		       n_querys * sizeof(DTYPE) / timer.time[0]);
+		    n_querys * sizeof(DTYPE) / timer.time[0]);
 		printf(" throughput_MOpps=%f latency_us=%f\n",
-		       n_querys / timer.time[0], timer.time[0]);
+		    n_querys / timer.time[0], timer.time[0]);
 #endif
 #endif
 	} else {
 		printf("[ERROR]\n");
 	}
-#endif				// WITH_BENCHMARK
+#endif // WITH_BENCHMARK
 
 #if NUMA
 	numa_free(input, input_size * sizeof(DTYPE));
