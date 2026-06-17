de3300c14a2b135127c84a775e6f4e98ca26ed4a
Birte Kristina Friesel <birte.friesel@uos.de>
2026-06-17 14:07:05 +0200

    perf: normalize events to #instr and group them together

--- include/perf.h
+++ include/perf.h
@@ -17,47 +17,66 @@ typedef struct {
 	char* desc;
 } perf_attr_t;
 
+/*
+ * Metrics used by bailey2014icpp:
+ * - L2D misses
+ * - L1D accesses
+ * - TLB misses
+ * - conditional branch instructions
+ * - vector instructions
+ * - stalled core cycles
+ * - total core cycles
+ * - reference cycles
+ * - idle FPU cycles
+ * - interrupts
+ * All of them normalized to #instr / #refcycles
+ */
+
+/*
+ * PERF_COUNT_HW_INSTRUCTIONS is used for normalization and must be the first
+ * item in the array.
+ */
 perf_attr_t perf_attrs[] = {
-	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS, "n_instr" },
-	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_REFERENCES, "n_cache_ref" },
-	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES, "n_cache_miss" },
-	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_INSTRUCTIONS, "n_branch" },
-	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES, "n_branch_miss" },
+	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS, "instr" },
+	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_REFERENCES, "cache_ref" },
+	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES, "cache_miss" },
+	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_INSTRUCTIONS, "branch" },
+	{ PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES, "branch_miss" },
 	/*
-	{PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_FRONTEND, "n_stalled_frontend"},
-	{PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_BACKEND,  "n_stalled_backend"},
+	{PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_FRONTEND, "stalled_frontend"},
+	{PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_BACKEND,  "stalled_backend"},
 	*/
 	{
 	    PERF_TYPE_HW_CACHE,
 	    PERF_COUNT_HW_CACHE_L1D
 	        | (PERF_COUNT_HW_CACHE_OP_READ << 8)
 	        | (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16),
-	    "n_l1d_read" },
+	    "l1d_read" },
 	{ PERF_TYPE_HW_CACHE,
 	    PERF_COUNT_HW_CACHE_L1D
 	        | (PERF_COUNT_HW_CACHE_OP_READ << 8)
 	        | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
-	    "n_l1d_read_miss" },
+	    "l1d_read_miss" },
 	{ PERF_TYPE_HW_CACHE,
 	    PERF_COUNT_HW_CACHE_LL
 	        | (PERF_COUNT_HW_CACHE_OP_READ << 8)
 	        | (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16),
-	    "n_llc_read" },
+	    "llc_read" },
 	{ PERF_TYPE_HW_CACHE,
 	    PERF_COUNT_HW_CACHE_LL
 	        | (PERF_COUNT_HW_CACHE_OP_READ << 8)
 	        | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
-	    "n_llc_read_miss" },
+	    "llc_read_miss" },
 	{ PERF_TYPE_HW_CACHE,
 	    PERF_COUNT_HW_CACHE_DTLB
 	        | (PERF_COUNT_HW_CACHE_OP_READ << 8)
 	        | (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16),
-	    "n_tlb_read" },
+	    "tlb_read" },
 	{ PERF_TYPE_HW_CACHE,
 	    PERF_COUNT_HW_CACHE_DTLB
 	        | (PERF_COUNT_HW_CACHE_OP_READ << 8)
 	        | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
-	    "n_tlb_read_miss" },
+	    "tlb_read_miss" },
 };
 
 perf_measurement_t* perf_measurements[sizeof(perf_attrs) / sizeof(perf_attr_t)];
@@ -79,11 +98,15 @@ perf_measurement_t* perf_alloc(int type, int config, unsigned long int attr_idx)
 
 void perf_start()
 {
+	int group = -1;
 	for (unsigned long int i = 0; i < sizeof(perf_attrs) / sizeof(perf_attr_t); i++) {
 		perf_measurements[i] = perf_alloc(perf_attrs[i].type, perf_attrs[i].config, i);
 	}
 	for (unsigned long int i = 0; i < sizeof(perf_attrs) / sizeof(perf_attr_t); i++) {
-		perf_open_measurement(perf_measurements[i], -1, 0);
+		perf_open_measurement(perf_measurements[i], group, 0);
+		if (i == 0) {
+			group = perf_measurements[0]->group;
+		}
 	}
 	for (unsigned long int i = 0; i < sizeof(perf_attrs) / sizeof(perf_attr_t); i++) {
 		perf_start_measurement(perf_measurements[i]);
@@ -104,8 +127,19 @@ void perf_stop()
 
 void perf_print()
 {
-	for (unsigned long int i = 0; i < sizeof(perf_attrs) / sizeof(perf_attr_t); i++) {
-		printf(" %s=%lu", perf_attrs[i].desc, measurements[i].values[0].value);
+	/*
+	 * By convention, perf_attrs[0] is #instr
+	 */
+	printf(" n_%s=%lu", perf_attrs[0].desc, measurements[0].values[0].value);
+	for (unsigned long int i = 1; i < sizeof(perf_attrs) / sizeof(perf_attr_t); i++) {
+#if PERF_PRINT_RAW_VALUES
+		printf(" n_%s=%lu", perf_attrs[i].desc, measurements[i].values[0].value);
+#else
+		/*
+		 * normalize to events per instruction
+		 */
+		printf(" ratio_%s=%f", perf_attrs[i].desc, (double)measurements[i].values[0].value / (double)measurements[0].values[0].value);
+#endif
 	}
 	printf("\n");
 }
