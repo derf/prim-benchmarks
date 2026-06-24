f9274176a886b1edfcecf93f34e47a16a466d616
Birte Kristina Friesel <birte.friesel@uos.de>
2026-06-24 11:09:11 +0200

    VA CPU: cleanup; perf support

--- VA/baselines/cpu/app_baseline.c
+++ VA/baselines/cpu/app_baseline.c
@@ -1,8 +1,3 @@
-/**
- * @file app.c
- * @brief Template for a Host Application Source File.
- *
- */
 #include <assert.h>
 #include <getopt.h>
 #include <stdbool.h>
@@ -14,8 +9,9 @@
 
 #include <omp.h>
 
-#if WITH_BENCHMARK
+#if DFATOOL_TIMING
 #include "../../include/timer.h"
+Timer timer;
 #else
 #define start(...)
 #define stop(...)
@@ -30,16 +26,12 @@
 #define perf_stop(...)
 #endif
 
-#if NUMA
-#include <numa.h>
-#include <numaif.h>
 
-void* mp_pages[1];
-int mp_status[1];
-int mp_nodes[1];
-int numa_node_in = -1;
-int numa_node_out = -1;
-int numa_node_cpu = -1;
+#if NUMA
+#include "../../../include/numa.h"
+#else
+#define numa_bind_alloc(size, bitmask) malloc(size)
+#define numa_free(data, size) free(data)
 #endif
 
 #define XSTR(x) STR(x)
@@ -53,27 +45,11 @@ static T* A;
 static T* B;
 static T* C;
 
-#if NUMA_MEMCPY
-int numa_node_cpu_memcpy = -1;
-int numa_node_local = -1;
-int numa_node_in_is_local = 0;
-static T* A_local;
-static T* B_local;
-#endif
-
-/**
- * @brief compute output in the host
- */
-static void vector_addition_host(unsigned long nr_elements, int t)
+static void vector_addition_host(unsigned long nr_elements)
 {
-	omp_set_num_threads(t);
 #pragma omp parallel for
 	for (long i = 0; i < nr_elements; i++) {
-#if NUMA_MEMCPY
-		C[i] = A_local[i] + B_local[i];
-#else
 		C[i] = A[i] + B[i];
-#endif
 	}
 }
 
@@ -89,10 +65,6 @@ typedef struct Params {
 	struct bitmask* bitmask_out;
 	int numa_node_cpu;
 #endif
-#if NUMA_MEMCPY
-	int numa_node_cpu_memcpy;
-	struct bitmask* bitmask_cpu;
-#endif
 } Params;
 
 void usage()
@@ -105,7 +77,6 @@ void usage()
 	    "\n    -t <T>    # of threads (default=8)"
 	    "\n    -w <W>    # of untimed warmup iterations (default=1)"
 	    "\n    -e <E>    # of timed repetition iterations (default=3)"
-	    "\n    -x <X>    Weak (0) or strong (1) scaling (default=0)"
 	    "\n"
 	    "\nBenchmark-specific options:"
 	    "\n    -i <I>    input size (default=8M elements)"
@@ -118,20 +89,15 @@ struct Params input_params(int argc, char** argv)
 	p.input_size = 16777216;
 	p.n_warmup = 1;
 	p.n_reps = 3;
-	p.exp = 1;
-	p.n_threads = 8;
+	p.n_threads = 4;
 #if NUMA
 	p.bitmask_in = NULL;
 	p.bitmask_out = NULL;
 	p.numa_node_cpu = -1;
 #endif
-#if NUMA_MEMCPY
-	p.numa_node_cpu_memcpy = -1;
-	p.bitmask_cpu = NULL;
-#endif
 
 	int opt;
-	while ((opt = getopt(argc, argv, "hi:w:e:x:t:a:b:c:C:M:")) >= 0) {
+	while ((opt = getopt(argc, argv, "hi:w:e:x:t:A:B:C:")) >= 0) {
 		switch (opt) {
 		case 'h':
 			usage();
@@ -146,30 +112,19 @@ struct Params input_params(int argc, char** argv)
 		case 'e':
 			p.n_reps = atoi(optarg);
 			break;
-		case 'x':
-			p.exp = atoi(optarg);
-			break;
 		case 't':
 			p.n_threads = atoi(optarg);
 			break;
 #if NUMA
-		case 'a':
+		case 'A':
 			p.bitmask_in = numa_parse_nodestring(optarg);
 			break;
-		case 'b':
+		case 'B':
 			p.bitmask_out = numa_parse_nodestring(optarg);
 			break;
-		case 'c':
+		case 'C':
 			p.numa_node_cpu = atoi(optarg);
 			break;
-#if NUMA_MEMCPY
-		case 'C':
-			p.bitmask_cpu = numa_parse_nodestring(optarg);
-			break;
-		case 'M':
-			p.numa_node_cpu_memcpy = atoi(optarg);
-			break;
-#endif // NUMA_MEMCPY
 #endif // NUMA
 		default:
 			fprintf(stderr, "\nUnrecognized option!\n");
@@ -190,96 +145,23 @@ int main(int argc, char** argv)
 
 	struct Params p = input_params(argc, argv);
 
-	const unsigned long input_size = p.exp == 0 ? p.input_size * p.n_threads : p.input_size;
+	const unsigned long input_size = p.input_size;
 
-	// Create an input file with arbitrary data.
-	/**
-	 * @brief creates a "test file" by filling a buffer of 64MB with pseudo-random values
-	 * @param nr_elements how many 32-bit elements we want the file to be
-	 * @return the buffer address
-	 */
-	srand(0);
-
-#if NUMA
-	if (p.bitmask_in) {
-		numa_set_membind(p.bitmask_in);
-		numa_free_nodemask(p.bitmask_in);
-	}
-	A = (T*)numa_alloc(input_size * sizeof(T));
-	B = (T*)numa_alloc(input_size * sizeof(T));
-#else
-	A = (T*)malloc(input_size * sizeof(T));
-	B = (T*)malloc(input_size * sizeof(T));
-#endif
-
-#if NUMA
-	if (p.bitmask_out) {
-		numa_set_membind(p.bitmask_out);
-		numa_free_nodemask(p.bitmask_out);
-	}
-	C = (T*)numa_alloc(input_size * sizeof(T));
-#else
-	C = (T*)malloc(input_size * sizeof(T));
-#endif
+	A = (T*) numa_bind_alloc(input_size * sizeof(T), p.bitmask_in);
+	B = (T*) numa_bind_alloc(input_size * sizeof(T), p.bitmask_in);
+	C = (T*) numa_bind_alloc(input_size * sizeof(T), p.bitmask_out);
 
 	omp_set_num_threads(p.n_threads);
+
+#if NUMA
+	numa_node_cpu = numa_cpu_bind(p.numa_node_cpu);
+#endif
+
 #pragma omp parallel for
 	for (unsigned long i = 0; i < input_size; i++) {
 		A[i] = (T)i % (1 << 31) + 5;
 		B[i] = (T)i % (1 << 31) + 6;
 	}
-
-#if NUMA
-#if NUMA_MEMCPY
-	if (p.bitmask_cpu) {
-		numa_set_membind(p.bitmask_cpu);
-		numa_free_nodemask(p.bitmask_cpu);
-	}
-#else
-	struct bitmask* bitmask_all = numa_allocate_nodemask();
-	numa_bitmask_setall(bitmask_all);
-	numa_set_membind(bitmask_all);
-	numa_free_nodemask(bitmask_all);
-#endif // NUMA_MEMCPY
-#endif // NUMA
-
-#if NUMA
-	mp_pages[0] = A;
-	if (move_pages(0, 1, mp_pages, NULL, mp_status, 0) == -1) {
-		perror("move_pages(A)");
-	} else if (mp_status[0] < 0) {
-		printf("move_pages error: %d", mp_status[0]);
-	} else {
-		numa_node_in = mp_status[0];
-	}
-
-	mp_pages[0] = C;
-	if (move_pages(0, 1, mp_pages, NULL, mp_status, 0) == -1) {
-		perror("move_pages(C)");
-	} else if (mp_status[0] < 0) {
-		printf("move_pages error: %d", mp_status[0]);
-	} else {
-		numa_node_out = mp_status[0];
-	}
-
-	numa_node_cpu = p.numa_node_cpu;
-	if (p.numa_node_cpu != -1) {
-		if (numa_run_on_node(p.numa_node_cpu) == -1) {
-			perror("numa_run_on_node");
-			numa_node_cpu = -1;
-		}
-	}
-#endif
-
-#if NUMA_MEMCPY
-	numa_node_in_is_local = ((numa_node_cpu == numa_node_in)
-	                            || (numa_node_cpu + 8 == numa_node_in))
-	    * 1;
-#endif
-
-#if WITH_BENCHMARK
-	Timer timer;
-#endif
 
 #if NOP_SYNC
 	for (int rep = 0; rep < 200000; rep++) {
@@ -289,68 +171,17 @@ int main(int argc, char** argv)
 
 	for (int rep = 0; rep < p.n_warmup + p.n_reps; rep++) {
 
-#if NUMA_MEMCPY
-		numa_node_cpu_memcpy = p.numa_node_cpu_memcpy;
-		start(&timer, 1, 0);
-		if (!numa_node_in_is_local) {
-			A_local = (T*)numa_alloc(input_size * sizeof(T));
-			B_local = (T*)numa_alloc(input_size * sizeof(T));
-		}
-		stop(&timer, 1);
-		if (!numa_node_in_is_local) {
-			if (p.numa_node_cpu_memcpy != -1) {
-				if (numa_run_on_node(p.numa_node_cpu_memcpy) == -1) {
-					perror("numa_run_on_node");
-					numa_node_cpu_memcpy = -1;
-				}
-			}
-		}
-		start(&timer, 2, 0);
-		if (!numa_node_in_is_local) {
-			memcpy(A_local, A, input_size * sizeof(T));
-			memcpy(B_local, B, input_size * sizeof(T));
-		} else {
-			A_local = A;
-			B_local = B;
-		}
-		stop(&timer, 2);
-		if (p.numa_node_cpu != -1) {
-			if (numa_run_on_node(p.numa_node_cpu) == -1) {
-				perror("numa_run_on_node");
-				numa_node_cpu = -1;
-			}
-		}
-		mp_pages[0] = A_local;
-		if (move_pages(0, 1, mp_pages, NULL, mp_status, 0) == -1) {
-			perror("move_pages(A_local)");
-		} else if (mp_status[0] < 0) {
-			printf("move_pages error: %d", mp_status[0]);
-		} else {
-			numa_node_local = mp_status[0];
-		}
-#endif
-
-		if (rep >= p.n_warmup) {
-			if (perf_start() != 0) {
-				return 1;
-			}
-		}
+		perf_start();
 		start(&timer, 0, 0);
-		vector_addition_host(input_size, p.n_threads);
-		stop(&timer, 0);
-		if (rep >= p.n_warmup) {
-			if (perf_stop() != 0) {
-				return 1;
-			}
-		}
 
-#if NUMA_MEMCPY
-		start(&timer, 3, 0);
-		if (!numa_node_in_is_local) {
-			numa_free(A_local, input_size * sizeof(T));
-			numa_free(B_local, input_size * sizeof(T));
-		}
-		stop(&timer, 3);
+		vector_addition_host(input_size);
+
+		stop(&timer, 0);
+		perf_stop();
+
+#if NUMA
+			numa_node_in = numa_get_node_of_page(A, "A");
+			numa_node_out = numa_get_node_of_page(C, "C");
 #endif
 
 		unsigned int nr_threads = 0;
@@ -358,60 +189,36 @@ int main(int argc, char** argv)
 #pragma omp atomic
 		nr_threads++;
 
-#if WITH_PERF_LIB
 		if (rep >= p.n_warmup) {
-			printf("[::] vector_addition_host | n_threads=%d e_type=%s n_elements=%ld"
+#if WITH_PERF_LIB
+			printf("[::] VA-CPU | n_threads=%d e_type=%s n_elements=%ld",
+			    nr_threads, XSTR(T), input_size);
 #if NUMA
-			       " numa_node_in=%d numa_node_out=%d numa_node_cpu=%d numa_distance_in_cpu=%d numa_distance_cpu_out=%d"
-#endif
-			       " |",
-			    nr_threads, XSTR(T), input_size
-#if NUMA
-			    ,
+			printf(" numa_node_in=%d numa_node_out=%d numa_node_cpu=%d numa_distance_in_cpu=%d numa_distance_cpu_out=%d",
 			    numa_node_in, numa_node_out, numa_node_cpu,
 			    numa_distance(numa_node_in, numa_node_cpu),
 			    numa_distance(numa_node_cpu, numa_node_out)
+				);
 #endif
-			);
+			printf(" |");
 			perf_print();
-		}
-#elif WITH_BENCHMARK
-		if (rep >= p.n_warmup) {
-#if NUMA_MEMCPY
-			printf("[::] VA-CPU-MEMCPY | n_threads=%d e_type=%s n_elements=%ld"
-			       " numa_node_in=%d numa_node_local=%d numa_node_out=%d numa_node_cpu=%d numa_node_cpu_memcpy=%d numa_distance_in_cpu=%d numa_distance_cpu_out=%d"
-			       " | throughput_MBps=%f",
-			    nr_threads, XSTR(T),
-			    input_size, numa_node_in, numa_node_local,
-			    numa_node_out, numa_node_cpu, numa_node_cpu_memcpy,
-			    numa_distance(numa_node_in, numa_node_cpu),
-			    numa_distance(numa_node_cpu, numa_node_out),
-			    input_size * 3 * sizeof(T) / timer.time[0]);
-			printf(" throughput_MOpps=%f",
-			    input_size / timer.time[0]);
-			printf(" latency_kernel_us=%f latency_alloc_us=%f latency_memcpy_us=%f latency_free_us=%f latency_total_us=%f\n",
-			    timer.time[0], timer.time[1], timer.time[2],
-			    timer.time[3],
-			    timer.time[0] + timer.time[1] + timer.time[2] + timer.time[3]);
-#else
-			printf("[::] VA-CPU | n_threads=%d e_type=%s n_elements=%ld"
+#elif DFATOOL_TIMING
+			printf("[::] VA-CPU | n_threads=%d e_type=%s n_elements=%ld",
+			    nr_threads, XSTR(T), input_size
+				);
 #if NUMA
-			       " numa_node_in=%d numa_node_out=%d numa_node_cpu=%d numa_distance_in_cpu=%d numa_distance_cpu_out=%d"
-#endif
-			       " | throughput_MBps=%f",
-			    nr_threads, XSTR(T), input_size,
-#if NUMA
+			printf(" numa_node_in=%d numa_node_out=%d numa_node_cpu=%d numa_distance_in_cpu=%d numa_distance_cpu_out=%d",
 			    numa_node_in, numa_node_out, numa_node_cpu,
 			    numa_distance(numa_node_in, numa_node_cpu),
-			    numa_distance(numa_node_cpu, numa_node_out),
+			    numa_distance(numa_node_cpu, numa_node_out)
+				);
 #endif
-			    input_size * 3 * sizeof(T) / timer.time[0]);
-			printf(" throughput_MOpps=%f",
-			    input_size / timer.time[0]);
-			printf(" latency_us=%f\n", timer.time[0]);
-#endif // NUMA_MEMCPY
+			printf(" | throughput_MBps=%f throughput_MOpps=%f latency_us=%f\n",
+			    input_size * 3 * sizeof(T) / timer.time[0],
+			    input_size / timer.time[0],
+				timer.time[0]);
+#endif // DFATOOL_TIMING
 		}
-#endif // WITH_BENCHMARK
 	}
 
 #if NOP_SYNC
@@ -420,15 +227,9 @@ int main(int argc, char** argv)
 	}
 #endif
 
-#if NUMA
 	numa_free(A, input_size * sizeof(T));
 	numa_free(B, input_size * sizeof(T));
 	numa_free(C, input_size * sizeof(T));
-#else
-	free(A);
-	free(B);
-	free(C);
-#endif
 
 	return 0;
 }
--- VA/baselines/cpu/run-ws.sh
+++ VA/baselines/cpu/run-ws.sh
@@ -1,6 +1,17 @@
 #!/bin/zsh
 
 make -B benchmark=0 debug=1 native=0 nop_sync=1 numa=1
+mkdir -p log/ws
 
-~/var/source/valgrind/vg-in-place --tool=ws --ws-file=t1.ws --ws-peak-detect=yes --ws-every=50000 --ws-track-locality=yes ./va -a 4 -b 4 -c 4 -t 1 -e 20 -w 0 -i 16777216
-~/var/source/valgrind/vg-in-place --tool=ws --ws-file=t4.ws --ws-peak-detect=yes --ws-every=50000 --ws-track-locality=yes ./va -a 4 -b 4 -c 4 -t 4 -e 20 -w 0 -i 16777216
+rm -f log/ws/*.ws.*(N)
+
+set -x
+
+for nthreads in 1 2 4 8 12 16; do
+	~/var/source/valgrind/vg-in-place --tool=ws --ws-file=log/ws/t${nthreads}.ws --ws-peak-detect=yes --ws-every=50000 --ws-track-locality=yes ./va -a 4 -b 4 -c 4 -t 1 -e 20 -w 0 -i 16777216
+done
+
+for wsfile in log/ws/*.ws.*; do
+	~/var/ess/aemr/dfatool/bin/extract-kernel-ws.py app_baseline.c $wsfile --output-format valgrind-ws > log/ws/kernel-${wsfile:t:r}
+	~/var/ess/aemr/dfatool/bin/extract-kernel-ws.py app_baseline.c $wsfile --output-format dfatool > log/ws/kernel-${wsfile:t:r:r}.txt
+done
