32b986712e638891be15d09048d706dd3e966231
Birte Kristina Friesel <birte.friesel@uos.de>
2026-06-24 11:17:04 +0200

    VA CPU: update benchmarks

--- VA/benchmark-scripts/milos-hbm-cxl-perf.sh
+++ VA/benchmark-scripts/milos-hbm-cxl-perf.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 cd baselines/cpu
-make -B benchmark=0 numa=1 perf_lib=1
+make -B dfatool_timing=0 numa=1 perf_lib=1
 
 mkdir -p log/$(hostname)
 fn=log/$(hostname)/milos-hbm-cxl-perf
 
+# 2^29 * 4B == 2 GiB per array → max 6 GiB total
 parallel -j1 --eta --joblog ${fn}.joblog --header : \
-	./va -i {input_size} -a {ram_in} -b {ram_out} -c {cpu} -t {nr_threads} -w 1 -e 20 \
+	./va -i {input_size} -t {nr_threads} -A {ram_in} -B {ram_out} -C {cpu} -w 1 -e 20 \
 	::: nr_threads 1 2 4 8 12 16 \
-	:::   ram_in 0 \
-	:::      cpu 0 \
-	:::+ ram_out 0 \
-	::: input_size 167772160 \
+	:::   ram_in 4 \
+	:::      cpu 4 \
+	:::+ ram_out 4 \
+	::: input_size $(( 2 ** 29 )) \
 > ${fn}.txt
 
 xz -f -v -9 ${fn}.txt
--- VA/benchmark-scripts/milos-hbm-cxl.sh
+++ VA/benchmark-scripts/milos-hbm-cxl.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 cd baselines/cpu
-make -B benchmark=1 numa=1 perf_lib=0
+make -B dfatool_timing=1 numa=1 perf_lib=0
 
 mkdir -p log/$(hostname)
 fn=log/$(hostname)/milos-hbm-cxl
 
+# 2^29 * 4B == 2 GiB per array → max 6 GiB total
 parallel -j1 --eta --joblog ${fn}.joblog --header : \
-	./va -i {input_size} -a {ram_in} -b {ram_out} -c {cpu} -t {nr_threads} -w 1 -e 5 \
+	./va -i {input_size} -t {nr_threads} -A {ram_in} -B {ram_out} -C {cpu} -w 1 -e 5 \
 	::: nr_threads 1 2 4 8 12 16 \
 	:::   ram_in $(seq 0 17) \
 	:::      cpu $(seq 0 7) $(seq 0 7) 0 4 \
 	:::+ ram_out $(seq 0 17) \
-	::: input_size 167772160 \
+	::: input_size $(( 2 ** 29 )) \
 > ${fn}.txt
 
 xz -f -v -9 ${fn}.txt
