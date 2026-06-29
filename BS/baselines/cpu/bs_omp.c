da2d58c66d25174ab59b0408e01596d8aaf98368
Birte Kristina Friesel <birte.friesel@uos.de>
2026-06-29 14:40:31 +0200

    BS: cleanup; built-in benchmark repetitions

--- BS/baselines/cpu/bs_omp.c
+++ BS/baselines/cpu/bs_omp.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <getopt.h>
+#include <omp.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -10,6 +11,7 @@
 
 #if DFATOOL_TIMING
 #include "../../include/timer.h"
+Timer timer;
 #else
 #define start(...)
 #define stop(...)
@@ -23,30 +25,78 @@
 #endif
 
 #if NUMA
-#include <numa.h>
-#include <numaif.h>
-
-void* mp_pages[1];
-int mp_status[1];
-int mp_nodes[1];
-struct bitmask* bitmask_in;
-int numa_node_in = -1;
-int numa_node_cpu = -1;
-#endif
-
-#if NUMA_MEMCPY
-struct bitmask* bitmask_cpu;
-int numa_node_cpu_memcpy = -1;
-int numa_node_local = -1;
-int numa_node_in_is_local = 0;
+#include "../../../include/numa.h"
+#else
+#define numa_bind_alloc(size, bitmask) malloc(size)
+#define numa_free(data, size) free(data)
 #endif
 
 #define DTYPE uint64_t
-/*
- * @brief creates a "test file" by filling a bufferwith values
- */
-void create_test_file(DTYPE* input, uint64_t nr_elements, DTYPE* querys,
-    uint64_t n_querys)
+
+typedef struct Params {
+	unsigned long input_size;
+	unsigned long n_queries;
+	int n_warmup;
+	int n_reps;
+	int n_threads;
+#if NUMA
+	struct bitmask* bitmask_in;
+	int numa_node_cpu;
+#endif
+} Params;
+
+struct Params p;
+
+volatile DTYPE result_host = -1;
+
+void input_params(int argc, char** argv)
+{
+	p.input_size = 262144;
+	p.n_queries = 16777216;
+	p.n_warmup = 1;
+	p.n_reps = 3;
+	p.n_threads = 4;
+#if NUMA
+	p.bitmask_in = NULL;
+	p.numa_node_cpu = -1;
+#endif
+
+	int opt;
+	while ((opt = getopt(argc, argv, "i:q:w:e:t:A:C:")) >= 0) {
+		switch (opt) {
+		case 'i':
+			p.input_size = atol(optarg);
+			break;
+		case 'q':
+			p.n_queries = atol(optarg);
+			break;
+		case 'w':
+			p.n_warmup = atoi(optarg);
+			break;
+		case 'e':
+			p.n_reps = atoi(optarg);
+			break;
+		case 't':
+			p.n_threads = atoi(optarg);
+			break;
+#if NUMA
+		case 'A':
+			p.bitmask_in = numa_parse_nodestring(optarg);
+			break;
+		case 'C':
+			p.numa_node_cpu = atoi(optarg);
+			break;
+#endif
+		default:
+			fprintf(stderr, "\nUnrecognized option!\n");
+			exit(0);
+		}
+	}
+	assert(p.n_threads > 0);
+}
+
+void create_test_file(DTYPE* input, uint64_t nr_elements, DTYPE* queries,
+    uint64_t n_queries)
 {
 
 	srand(time(NULL));
@@ -56,35 +106,32 @@ void create_test_file(DTYPE* input, uint64_t nr_elements, DTYPE* querys,
 		input[i] = input[i - 1] + (rand() % 10) + 1;
 	}
 
-	for (uint64_t i = 0; i < n_querys; i++) {
-		querys[i] = input[rand() % (nr_elements - 2)];
+	for (uint64_t i = 0; i < n_queries; i++) {
+		queries[i] = input[rand() % (nr_elements - 2)];
 	}
 }
 
-/**
- * @brief compute output in the host
- */
-uint64_t binarySearch(DTYPE* input, uint64_t input_size, DTYPE* querys,
-    unsigned n_querys)
+uint64_t binarySearch(DTYPE* input, uint64_t input_size, DTYPE* queries,
+    unsigned n_queries)
 {
 
 	uint64_t found = -1;
 	uint64_t q, r, l, m;
 
 #pragma omp parallel for private(q, r, l, m)
-	for (q = 0; q < n_querys; q++) {
+	for (q = 0; q < n_queries; q++) {
 		l = 0;
 		r = input_size;
 		while (l <= r) {
 			m = l + (r - l) / 2;
 
 			// Check if x is present at mid
-			if (input[m] == querys[q]) {
+			if (input[m] == queries[q]) {
 				found += m;
 				break;
 			}
 			// If x greater, ignore left half
-			if (input[m] < querys[q])
+			if (input[m] < queries[q])
 				l = m + 1;
 
 			// If x is smaller, ignore right half
@@ -96,137 +143,25 @@ uint64_t binarySearch(DTYPE* input, uint64_t input_size, DTYPE* querys,
 	return found;
 }
 
-/**
- * @brief Main of the Host Application.
- */
 int main(int argc, char** argv)
 {
-#if DFATOOL_TIMING
-	Timer timer;
-#endif
 
-	if (argc < 3) {
-		printf("Usage: ./bs_omp <input_size> <n_queries>\n");
-		return 1;
-	}
-
-	uint64_t input_size = atol(argv[1]);
-	uint64_t n_querys = atol(argv[2]);
+	input_params(argc, argv);
 
 #if NUMA
-	if (argc < 5) {
-		printf("Usage: ./bs_omp <input_size> <n_queries> <numa_data_in> <numa_compute>\n");
-		return 1;
-	}
-	bitmask_in = numa_parse_nodestring(argv[3]);
-	numa_node_cpu = atoi(argv[4]);
-#endif
-#if NUMA_MEMCPY
-	if (argc < 7) {
-		printf("Usage: ./bs_omp <input_size> <n_queries> <numa_data_in> <numa_compute> <numa_memcpy> <numa_memcpy>\n");
-		return 1;
-	}
-	bitmask_cpu = numa_parse_nodestring(argv[5]);
-	numa_node_cpu_memcpy = atoi(argv[6]);
+	numa_node_cpu = numa_cpu_bind(p.numa_node_cpu);
 #endif
 
-	printf("Vector size: %lu, num searches: %lu\n", input_size, n_querys);
+	omp_set_num_threads(p.n_threads);
 
-#if NUMA
-	if (bitmask_in) {
-		numa_set_membind(bitmask_in);
-		numa_free_nodemask(bitmask_in);
-	}
-	DTYPE* input = numa_alloc(input_size * sizeof(DTYPE));
-	DTYPE* querys = numa_alloc(n_querys * sizeof(DTYPE));
-#else
-	DTYPE* input = malloc(input_size * sizeof(DTYPE));
-	DTYPE* querys = malloc(n_querys * sizeof(DTYPE));
-#endif
-
-#if NUMA
-#if NUMA_MEMCPY
-	if (bitmask_cpu) {
-		numa_set_membind(bitmask_cpu);
-		numa_free_nodemask(bitmask_cpu);
-	}
-#else
-	struct bitmask* bitmask_all = numa_allocate_nodemask();
-	numa_bitmask_setall(bitmask_all);
-	numa_set_membind(bitmask_all);
-	numa_free_nodemask(bitmask_all);
-#endif // NUMA_MEMCPY
-#endif
-
-	DTYPE result_host = -1;
+	DTYPE* input = numa_bind_alloc(p.input_size * sizeof(DTYPE), p.bitmask_in);
+	DTYPE* queries = numa_bind_alloc(p.n_queries * sizeof(DTYPE), p.bitmask_in);
 
 	// Create an input file with arbitrary data.
-	create_test_file(input, input_size, querys, n_querys);
+	create_test_file(input, p.input_size, queries, p.n_queries);
 
 #if NUMA
-	mp_pages[0] = input;
-	if (move_pages(0, 1, mp_pages, NULL, mp_status, 0) == -1) {
-		perror("move_pages(A)");
-	} else if (mp_status[0] < 0) {
-		printf("move_pages error: %d", mp_status[0]);
-	} else {
-		numa_node_in = mp_status[0];
-	}
-
-	if (numa_node_cpu != -1) {
-		if (numa_run_on_node(numa_node_cpu) == -1) {
-			perror("numa_run_on_node");
-			numa_node_cpu = -1;
-		}
-	}
-#endif
-
-#if NUMA_MEMCPY
-	numa_node_in_is_local = ((numa_node_cpu == numa_node_in)
-	                            || (numa_node_cpu + 8 == numa_node_in))
-	    * 1;
-#endif
-
-#if NUMA_MEMCPY
-	DTYPE* input_local = input;
-	DTYPE* querys_local = querys;
-	start(&timer, 1, 0);
-	if (!numa_node_in_is_local) {
-		input_local = numa_alloc((input_size) * sizeof(DTYPE));
-		querys_local = numa_alloc((n_querys) * sizeof(DTYPE));
-	}
-	stop(&timer, 1);
-	if (!numa_node_in_is_local) {
-		if (numa_node_cpu_memcpy != -1) {
-			if (numa_run_on_node(numa_node_cpu_memcpy) == -1) {
-				perror("numa_run_on_node");
-				numa_node_cpu_memcpy = -1;
-			}
-		}
-	}
-	start(&timer, 2, 0);
-	if (!numa_node_in_is_local) {
-		memcpy(input_local, input, input_size * sizeof(DTYPE));
-		memcpy(querys_local, querys, n_querys * sizeof(DTYPE));
-	} else {
-		input_local = input;
-		querys_local = querys;
-	}
-	stop(&timer, 2);
-	if (numa_node_cpu != -1) {
-		if (numa_run_on_node(numa_node_cpu) == -1) {
-			perror("numa_run_on_node");
-			numa_node_cpu = -1;
-		}
-	}
-	mp_pages[0] = input_local;
-	if (move_pages(0, 1, mp_pages, NULL, mp_status, 0) == -1) {
-		perror("move_pages(input_local)");
-	} else if (mp_status[0] < 0) {
-		printf("move_pages error: %d", mp_status[0]);
-	} else {
-		numa_node_local = mp_status[0];
-	}
+	numa_node_in = numa_get_node_of_page(input, "input");
 #endif
 
 #if NOP_SYNC
@@ -235,91 +170,56 @@ int main(int argc, char** argv)
 	}
 #endif
 
-	perf_start();
-	start(&timer, 0, 0);
-#if NUMA_MEMCPY
-	result_host = binarySearch(input_local, input_size - 1, querys_local, n_querys);
-#else
-	result_host = binarySearch(input, input_size - 1, querys, n_querys);
-#endif
-	stop(&timer, 0);
-	perf_stop();
+	for (int rep = 0; rep < p.n_warmup + p.n_reps; rep++) {
+		perf_start();
+		start(&timer, 0, 0);
+		result_host = binarySearch(input, p.input_size - 1, queries, p.n_queries);
+		stop(&timer, 0);
+		perf_stop();
 
 #if NOP_SYNC
-	for (int rep = 0; rep < 200000; rep++) {
-		asm volatile("nop" ::);
-	}
+		for (int rep = 0; rep < 200000; rep++) {
+			asm volatile("nop" ::);
+		}
 #endif
 
-#if NUMA_MEMCPY
-	start(&timer, 3, 0);
-	if (!numa_node_in_is_local) {
-		numa_free(input_local, input_size * sizeof(DTYPE));
-		numa_free(querys_local, n_querys * sizeof(DTYPE));
-	}
-	stop(&timer, 3);
-#endif
-
-	int status = (result_host);
-	unsigned int nr_threads = 0;
+		int status = (result_host);
+		unsigned int nr_threads = 0;
 #pragma omp parallel
 #pragma omp atomic
-	nr_threads++;
+		nr_threads++;
 
-	if (status) {
+		if (status) {
+			if (rep >= p.n_warmup) {
 #if WITH_PERF_LIB
-		printf("[::] binarySearch | n_threads=%d e_type=%s n_elements=%lu"
+				printf("[::] binarySearch | n_threads=%d e_type=%s n_elements=%lu"
+				       " |",
+				    nr_threads, "uint64_t", p.input_size);
 #if NUMA
-		       " numa_node_in=%d numa_node_cpu=%d numa_distance_in_cpu=%d"
+				printf(" numa_node_in=%d numa_node_cpu=%d numa_distance_in_cpu=%d", numa_node_in, numa_node_cpu, numa_distance(numa_node_in, numa_node_cpu));
 #endif
-		       " |",
-		    nr_threads, "uint64_t", input_size
-#if NUMA
-		    ,
-		    numa_node_in, numa_node_cpu,
-		    numa_distance(numa_node_in, numa_node_cpu)
-#endif
-		);
-		perf_print();
+				printf(" |");
+				perf_print();
 #elif DFATOOL_TIMING
-#if NUMA_MEMCPY
-		printf("[::] BS-CPU-MEMCPY | n_threads=%d e_type=%s n_elements=%lu"
-		       " numa_node_in=%d numa_node_local=%d numa_node_cpu=%d numa_node_cpu_memcpy=%d numa_distance_in_cpu=%d"
-		       " | throughput_MBps=%f throughput_MOpps=%f"
-		       " latency_kernel_us=%f latency_alloc_us=%f latency_memcpy_us=%f latency_free_us=%f latency_total_us=%f\n",
-		    nr_threads, "uint64_t", input_size, numa_node_in,
-		    numa_node_local, numa_node_cpu, numa_node_cpu_memcpy,
-		    numa_distance(numa_node_in, numa_node_cpu),
-		    n_querys * sizeof(DTYPE) / timer.time[0],
-		    n_querys / timer.time[0], timer.time[0], timer.time[1],
-		    timer.time[2], timer.time[3],
-		    timer.time[0] + timer.time[1] + timer.time[2] + timer.time[3]);
-#else
-		printf("[::] BS-CPU | n_threads=%d e_type=%s n_elements=%lu"
+				printf("[::] BS-CPU | n_threads=%d e_type=%s n_elements=%lu",
+				    nr_threads, "uint64_t", p.input_size);
 #if NUMA
-		       " numa_node_in=%d numa_node_cpu=%d numa_distance_in_cpu=%d"
+				printf(" numa_node_in=%d numa_node_cpu=%d numa_distance_in_cpu=%d", numa_node_in, numa_node_cpu, numa_distance(numa_node_in, numa_node_cpu));
 #endif
-		       " | throughput_MBps=%f",
-		    nr_threads, "uint64_t", input_size,
-#if NUMA
-		    numa_node_in, numa_node_cpu, numa_distance(numa_node_in, numa_node_cpu),
-#endif
-		    n_querys * sizeof(DTYPE) / timer.time[0]);
-		printf(" throughput_MOpps=%f latency_us=%f\n",
-		    n_querys / timer.time[0], timer.time[0]);
-#endif // NUMA_MEMCPY
+				printf(" | throughput_MBps=%f",
+				    p.n_queries * sizeof(DTYPE) / timer.time[0]);
+				printf(" throughput_MOpps=%f latency_us=%f\n",
+				    p.n_queries / timer.time[0], timer.time[0]);
 #endif // DFATOOL_TIMING
-	} else {
-		printf("[ERROR]\n");
+			}
+		} else {
+			printf("[ERROR]\n");
+			return 1;
+		}
 	}
 
-#if NUMA
-	numa_free(input, input_size * sizeof(DTYPE));
-	numa_free(querys, n_querys * sizeof(DTYPE));
-#else
-	free(input);
-	free(querys);
-#endif
+	numa_free(input, p.input_size * sizeof(DTYPE));
+	numa_free(queries, p.n_queries * sizeof(DTYPE));
 
-	return status ? 0 : 1;
+	return 0;
 }
--- BS/benchmark-scripts/milos-hbm-cxl-perf.sh
+++ BS/benchmark-scripts/milos-hbm-cxl-perf.sh
@@ -12,20 +12,8 @@ input_size=$((2**28))
 # 2^22 queries * 8 B == 32 MiB
 num_queries=$((2**22))
 
-run_benchmark() {
-	local "$@"
-	set -e
-	# warmup
-	OMP_NUM_THREADS=${nr_threads} ./bs_omp ${input_size} ${num_queries} $ram $cpu > /dev/null
-	for i in $(seq 1 20); do
-		OMP_NUM_THREADS=${nr_threads} ./bs_omp ${input_size} ${num_queries} $ram $cpu
-	done
-}
-
-export -f run_benchmark
-
 parallel -j1 --eta --joblog ${fn}.joblog --header : \
-	run_benchmark nr_threads={nr_threads} ram={ram} cpu={cpu} input_size=${input_size} num_queries=${num_queries} \
+	./bs_omp -t {nr_threads} -w 1 -e 20 -A {ram} -C {cpu} -i ${input_size} -q ${num_queries} \
 		::: nr_threads 1 2 4 8 12 16 \
 		::: cpu 4 \
 		::: ram 4 \
